1b0a14c9f40406be4757e483b431659c0c0f4981
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2016-09-14 09:10:38 +0100

    .

--- ParameterVector.cpp
+++ ParameterVector.cpp
@@ -15,3 +15,28 @@ ParameterVector::ParameterVector(ParameterScalar* buffer, size_t length, int var
 
 ParameterVector::~ParameterVector() {}
 
+
+ParameterVector& ParameterVector::operator=(const ParameterVector& other)
+{
+ return *this;
+}
+
+ParameterVector& ParameterVector::operator *=(ParameterScalar a)
+{
+    if (this->buffer_!=nullptr) { //both in memory
+        for (size_t k=0; k<length_; k++) buffer_[k] *= a;
+    }
+    else throw std::logic_error("implementation incomplete");
+ return *this;
+}
+
+ParameterScalar ParameterVector::operator*(const ParameterVector& other) const
+{
+    ParameterScalar result=0;
+    if (this->buffer_!=nullptr && other.buffer_!=nullptr) { // both in memory
+        for (size_t k=0; k<length_; k++) result += buffer_[k]*other.buffer_[k];
+    }
+    else throw std::logic_error("implementation incomplete");
+    return result;
+}
+
--- ParameterVector.h
+++ ParameterVector.h
@@ -3,6 +3,9 @@
 
 #include <cstddef>
 #include <vector>
+#ifndef nullptr
+#define nullptr NULL
+#endif
 
 
 namespace IterativeSolver {
@@ -11,9 +14,53 @@ namespace IterativeSolver {
   class ParameterVector
   {
   public:
-    ParameterVector(int variance=0);
+      /*!
+     * \brief Construct an object from provided data. The data buffer will be assumed to continue
+     * to exist whilst this object exists, but may have its contents changed externally.
+     * \param buffer The data buffer.
+     * \param length The length of the data buffer.
+     * \param variance
+     * - -1: covariant vector
+     * - +1: contravariant vector
+     * - 0: self-dual vector
+     * The class is expected to check that appropriate combinations of vectors are provided in methods that perform linear algebra functions.
+     */
     ParameterVector(ParameterScalar* buffer, size_t length, int variance=0);
+      /*!
+     * \brief Construct an object without any data. Subsequent assignment of data to the object via the overloaded = operator should result in the data
+     * being stored internally, either in memory or on external storage.
+     * \param variance
+     * - -1: covariant vector
+     * - +1: contravariant vector
+     * - 0: self-dual vector
+     * The class is expected to check that appropriate combinations of vectors are provided in methods that perform linear algebra functions.
+     */
+    ParameterVector(int variance=0);
     virtual ~ParameterVector();
+    /*!
+     * \brief Calculate the residual for equations to be solved.
+     * THIS IS MAYBE NOT GOOD DESIGN SINCE THE CLASS THEN DEPENDS ON WHAT SORT OF EQUATIONS ARE TO BE SOLVED.
+     * \param g On exit, will contain the residual added to any previous contents of g
+     */
+    virtual void residual(ParameterVector & g) const = 0;
+    /*!
+     * \brief Multiply by scalar
+     * \param a The factor to multiply.
+     * \return
+     */
+    virtual ParameterVector& operator*=(ParameterScalar a);
+    /*!
+     * \brief Scalar product of two objects.
+     * \param other The object to be contracted with this.
+     * \return
+     */
+    virtual ParameterScalar operator*(const ParameterVector& other) const;
+    /*!
+     * \brief Copy from one object to another, allocating storage if needed
+     * \param other The source of data.
+     * \return
+     */
+    virtual ParameterVector& operator=(const ParameterVector& other);
   private:
     int variance_;
     size_t length_;

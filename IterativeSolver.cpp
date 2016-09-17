8c772d281eb40d63cb2b93de4595bb729ef251b9
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2016-09-17 16:11:33 +0100

    tidy

--- IterativeSolver.cpp
+++ IterativeSolver.cpp
@@ -13,58 +13,42 @@ IterativeSolverBase::~IterativeSolverBase()
 }
 
 
-#include <limits>
-
 bool IterativeSolverBase::iterate(ParameterVectorSet &residual, ParameterVectorSet &solution, ParameterVectorSet &other, std::string options)
 {
-	m_residuals.push_back(residual); m_solutions.push_back(solution); m_others.push_back(other);
-	m_lastVectorIndex=m_residuals.size()-1; // derivative classes might eventually store the vectors on top of previous ones, in which case they will need to store the position here for later calculation of iteration step
-    if (m_verbosity>3) {
-	  std::cout <<"added to m_residuals, now size="<<m_residuals.size()<<std::endl;
-      std::cout << "latest residual: "<<m_residuals.back()<<std::endl;
-      std::cout << "latest solution: "<<m_solutions.back()<<std::endl;
-    }
-    extrapolate(residual,solution,other,options);
-//	  std::cout << "after extrapolate solution: "<<solution<<std::endl;
-	m_updateFunction(residual,solution,std::vector<ParameterScalar>());
-    calculateErrors(solution);
-//    std::cout << "iterate() final extrapolated and updated solution: "<<solution<<std::endl;
-//    std::cout << "iterate() final extrapolated and updated residual: "<<residual<<std::endl;
-//    std::cout << "error: "<<err<<", m_thresh="<<m_thresh<<std::endl;
-        return m_error < m_thresh;
+  m_residuals.push_back(residual); m_solutions.push_back(solution); m_others.push_back(other);
+  m_lastVectorIndex=m_residuals.size()-1; // derivative classes might eventually store the vectors on top of previous ones, in which case they will need to store the position here for later calculation of iteration step
+  extrapolate(residual,solution,other,options);
+  m_updateFunction(residual,solution,std::vector<ParameterScalar>());
+  calculateErrors(solution);
+  return m_error < m_thresh;
 }
 
 
- void IterativeSolverBase::extrapolate(ParameterVectorSet & residual, ParameterVectorSet & solution, ParameterVectorSet & other, std::string options)
- {
+void IterativeSolverBase::extrapolate(ParameterVectorSet & residual, ParameterVectorSet & solution, ParameterVectorSet & other, std::string options)
+{
 
- }
+}
 
- bool IterativeSolverBase::solve(ParameterVectorSet & residual, ParameterVectorSet & solution, std::string options)
- {
-         bool converged=false;
-         size_t max_iterations=1000; //FIXME
-     for (size_t iteration=1; iteration <= m_maxIterations && not converged; iteration++) {
-         m_residualFunction(solution,residual,std::vector<double>());
-         converged = iterate(residual,solution);
-         if (m_verbosity>0)
-         std::cout << "iteration "<<iteration<<", error["<<m_worst<<"] = "<<m_error
-         <<std::endl;
-     }
-   return converged;
- }
+bool IterativeSolverBase::solve(ParameterVectorSet & residual, ParameterVectorSet & solution, std::string options)
+{
+  bool converged=false;
+  size_t max_iterations=1000; //FIXME
+  for (size_t iteration=1; iteration <= m_maxIterations && not converged; iteration++) {
+      m_residualFunction(solution,residual,std::vector<double>());
+      converged = iterate(residual,solution);
+      if (m_verbosity>0)
+        std::cout << "iteration "<<iteration<<", error["<<m_worst<<"] = "<<m_error <<std::endl;
+    }
+  return converged;
+}
 
 void IterativeSolverBase::calculateErrors(const ParameterVectorSet &solution)
 {
   ParameterVectorSet step=solution;
   step.axpy(-1,m_solutions[m_lastVectorIndex]);
-//  std::cout << "m_solutions[m_lastVectorIndex]="<<m_solutions[m_lastVectorIndex]<<std::endl;
-//  std::cout << "solution="<<solution<<std::endl;
-//  std::cout << "m_residuals[m_lastVectorIndex]="<<m_residuals[m_lastVectorIndex]<<std::endl;
-//  std::cout << "step="<<step<<std::endl;
   m_errors.clear();
-    for (size_t k=0; k<solution.size(); k++)
-        m_errors.push_back(m_residuals[m_lastVectorIndex].active[k] ? std::fabs(m_residuals[m_lastVectorIndex][k]*step[k]) : 0);
-    m_error = *max_element(m_errors.begin(),m_errors.end());
-    m_worst = max_element(m_errors.begin(),m_errors.end())-m_errors.begin();
+  for (size_t k=0; k<solution.size(); k++)
+    m_errors.push_back(m_residuals[m_lastVectorIndex].active[k] ? std::fabs(m_residuals[m_lastVectorIndex][k]*step[k]) : 0);
+  m_error = *max_element(m_errors.begin(),m_errors.end());
+  m_worst = max_element(m_errors.begin(),m_errors.end())-m_errors.begin();
 }

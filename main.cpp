8c59f1e0020478164a3d2c8a298009b0b6dd41b3
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2016-09-28 13:38:06 +0100

    Temporary clone of DIIS::

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include "Diis.h"
+#include "OldDiis.h"
 #include "Davidson.h"
 
 extern "C" { void IterativeSolverFTest();}
 int main(int argc, char *argv[])
 {
-  IterativeSolver::DIIS::test(1);
-  IterativeSolver::DIIS::test(1,6,1e6,IterativeSolver::DIIS::DIISmode,0.2);
+  IterativeSolver::OldDIIS::test(1);
+  IterativeSolver::OldDIIS::test(1,6,1e6,IterativeSolver::OldDIIS::DIISmode,0.2);
 //  IterativeSolver::DIIS::test(1,6,1e6,IterativeSolver::DIIS::disabled,0.0002);
-  IterativeSolver::DIIS::test(1,6,1e6,IterativeSolver::DIIS::DIISmode,0.4);
-  IterativeSolver::DIIS::test(1,6,1e6,IterativeSolver::DIIS::DIISmode,0.8);
-  IterativeSolver::DIIS::test(1,6,1e6,IterativeSolver::DIIS::DIISmode,0.8);
+  IterativeSolver::OldDIIS::test(1,6,1e6,IterativeSolver::OldDIIS::DIISmode,0.4);
+  IterativeSolver::OldDIIS::test(1,6,1e6,IterativeSolver::OldDIIS::DIISmode,0.8);
+  IterativeSolver::OldDIIS::test(1,6,1e6,IterativeSolver::OldDIIS::DIISmode,0.8);
 //  IterativeSolver::DIIS::test(1,6,1e6,IterativeSolver::DIIS::KAINmode);
   IterativeSolver::Davidson::test(9,1,1,0);
   IterativeSolver::Davidson::test(9,1,1,1);

8fe3f68871c337cdd7e8c3c0764c37fab204c3b0
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2016-11-03 08:56:50 +0000

    Update docs

--- IterativeSolver/LinearAlgebra.h
+++ IterativeSolver/LinearAlgebra.h
@@ -11,9 +11,9 @@
 namespace LinearAlgebra {
 
   /*!
-   * \brief An abstract base class for holding vectors.
+   * \brief An abstract base class for holding vectors with associated basic linear algebra operations.
    *
-   * Concrete implementations should implement all of the public methods, together with ensuring
+   * Concrete implementations should implement all of the virtual public methods, together with ensuring
    * that the
    * copy operator=() performs a deep copy to make a completely independent clone.
    * They must also define a copy method exactly as follows.
@@ -24,11 +24,14 @@ namespace LinearAlgebra {
    * storing
    * externally and/or distributed across processes, so long as all of the public methods function correctly.
    *
-   * The put(), get() and size() methods provided in this base class
-   * offer direct access to that data, for which there may be no meaningful implementation,
+   * The operator[](), put(), get() and size() methods provided in this base class
+   * offer an interface for direct access to the object's data,
+   * for which there might be no meaningful implementation,
    * so they should not normally be referenced. However they are defined so that
    * code that uses objects of this class can be tested in conjunction with a derivative
-   * class that does provide real implementations of these methods.
+   * class that does provide real implementations of these methods. Similarly, a dummy str()
+   * virtual method is provided, which can be accessed through the << operator with the intention
+   * of providing a printable representation of the object.
    */
   template<class scalar>
   class vector
@@ -44,21 +47,25 @@ namespace LinearAlgebra {
      * \return
      */
     virtual void axpy(scalar a, const vector<scalar>* other)=0;
+
     /*!
      * \brief Scalar product of two objects.
      * \param other The object to be contracted with this.
      * \return
      */
     virtual scalar dot(const vector<scalar>* other) const=0;
+
     /*!
      * \brief scal Scale the object by a factor.
      * \param a The factor to scale by.
      */
     virtual void scal(scalar a)=0;
+
     /*!
      * \brief Set the contents of the object to zero.
      */
     virtual void zero()=0;
+
     /*!
      * \brief Record the co/contra-variance status of the object
      * \param variance
@@ -68,6 +75,7 @@ namespace LinearAlgebra {
      * The class is expected to check that appropriate combinations of vectors are provided in methods that perform linear algebra functions.
      */
     void setVariance(int variance=0) {m_variance=variance;}
+
     /*!
      * \brief Report the co/contra-variance status of the object
      * \return
@@ -76,16 +84,19 @@ namespace LinearAlgebra {
      * - 0: self-dual vector
      */
     int variance() const {return m_variance;}
+
     /*!
      * \brief Make a copy of the object
      * \return A pointer to the new object
      */
     virtual vector<scalar>* clone() const=0;
+
     /*!
      * \brief Make a printable representation of the object
      * (optional implementation).
      * \return
      */
+
     virtual std::string str() const {return "";}
     /*!
      * \brief Set some of the object's data.
@@ -96,9 +107,8 @@ namespace LinearAlgebra {
      * \return
      */
     virtual void put(scalar* const buffer, size_t length, size_t offset)
-    {
-      throw std::logic_error("Unimplemented function");
-    }
+    { throw std::logic_error("Unimplemented function"); }
+
     /*!
      * \brief Fetch some of the object's data.
      * (optional implementation).
@@ -108,9 +118,13 @@ namespace LinearAlgebra {
      * \return
      */
     virtual void get(scalar* buffer, size_t length, size_t offset) const
-    {
-      throw std::logic_error("Unimplemented function");
-    }
+    { throw std::logic_error("Unimplemented function"); }
+
+    virtual scalar& operator[](size_t pos) const
+    { throw std::logic_error("Unimplemented function"); }
+    virtual scalar& operator[](size_t pos)
+    { return const_cast<scalar&> (static_cast<const vector*> (this)->operator[](pos)); }
+
     /*!
      * \brief Report the size of the object's data
      * (optional implementation).
@@ -120,7 +134,7 @@ namespace LinearAlgebra {
     {
       throw std::logic_error("Unimplemented function");
     }
-  protected:
+
   private:
     int m_variance;
   };
@@ -133,8 +147,15 @@ namespace LinearAlgebra {
   */
   template <class scalar>
   inline std::ostream& operator<<(std::ostream& os, const vector<scalar>* pv) { os << pv->str(); return os; }
+
+
+
   /*!
-   * \brief A container for a collection of vector objects
+   * \brief A container for a collection of LinearAlgebra::vector objects.
+   *
+   * This class does not need to be reimplemented for derivatives of LinearAlgebra::vector,
+   * but can simply be used as-is; vectors can be added by giving a pointer to push_back()
+   * (pointer to original data stored) or push_back_clone (copy of original data taken, and destroyed with the object).
    */
   template<class scalar>
   class vectorSet
@@ -181,6 +202,7 @@ namespace LinearAlgebra {
         }
       return *this;
     }
+
     /*!
      * \brief Add a constant times another object to this object
      * \param a The factor to multiply.
@@ -190,14 +212,15 @@ namespace LinearAlgebra {
     void axpy(scalar a, const vectorSet<scalar>& other) {
       for (size_t k=0; k<size(); k++) this->m_pvs[k]->axpy(a,other[k]);
     }
+
     /*!
      * \brief Set the contents of the object to zero.
      */
     void zero() { for (size_t k=0; k<size(); k++) this->m_pvs[k]->zero(); }
 
     /*!
-       * \brief A place to record whether each member of the set is meant to be considered active
-       */
+     * \brief A place to record whether each member of the set is meant to be considered active
+     */
     std::vector<bool> m_active;
   private:
     std::vector<bool> m_owned; ///< whether the vector objects pointed to are owned by this container (and therefore destroyed in the destructor)

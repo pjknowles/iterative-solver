e20c3329a7d4a10819d08da7d4f8c25c82576aca
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2018-05-25 08:50:06 +0100

    example

--- examples/LinearEigensystemExample.cpp
+++ examples/LinearEigensystemExample.cpp
@@ -1,78 +1,63 @@
-//#include "IterativeSolver/ISDavidson.h"
-//#include "IterativeSolver/SimpleParameterVector.h"
-//#include "IterativeSolver/PagedParameterVector.h"
-//#include "IterativeSolver/PagedVector.h"
+#include "IterativeSolver.h"
+#include "PagedVector.h"
 
-//using namespace LinearAlgebra;
 
-////  typedef SimpleParameterVector pv;
-////  typedef PagedParameterVector pv;
-//  typedef PagedVector pv;
+// Find lowest eigensolutions of M(i,j) = alpha*(i+1)*delta(i,j) + i + j
+using scalar = double;
+using pv = LinearAlgebra::PagedVector<scalar>;
+constexpr size_t n=100; // dimension of problem
+constexpr double alpha=100; // separation of diagonal elements
 
-//// Find lowest eigensolutions of M(i,j) = alpha*(i+1)*delta(i,j) + i + j
-//static size_t n; // dimension of problem
-//static double alpha; // separation of diagonal elements
+void action(const LinearAlgebra::vectorSet<scalar> & psx, LinearAlgebra::vectorSet<scalar> & outputs) {
+ std::vector<scalar> psxk(n);
+ std::vector<scalar> output(n);
+ for (size_t k=0; k<psx.size(); k++) {
+  psx[k]->get(&(psxk[0]),n,0);
+  for (size_t i=0; i<n; i++) {
+   output[i] = (alpha*(i+1))*psxk[i];
+   for (size_t j=0; j<n; j++)
+    output[i] += (i+j)*psxk[j];
+  }
+  outputs[k]->put(&output[0],n,0);
+ }
+}
 
-//struct : IterativeSolverBase::ParameterSetTransformation {
-//void operator()(const ParameterVectorSet & psx, ParameterVectorSet & outputs, std::vector<scalar> shift=std::vector<scalar>(), bool append=false) const override {
-//  std::vector<ParameterScalar> psxk(n);
-//  std::vector<ParameterScalar> output(n);
-//  for (size_t k=0; k<psx.size(); k++) {
-//      psx[k]->get(&(psxk[0]),n,0);
-//      if (append)
-//          outputs[k]->get(&output[0],n,0);
-//      else
-//        for (size_t i=0; i<n; i++) output[i]=0;
-//      for (size_t i=0; i<n; i++) {
-//            output[i] += (alpha*(i+1))*psxk[i];
-//            for (size_t j=0; j<n; j++)
-//                output[i] += (i+j)*psxk[j];
-//        }
-//      outputs[k]->put(&output[0],n,0);
-//    }
-//}
-//} _matrix_residual;
-
-//struct : IterativeSolverBase::ParameterSetTransformation {
-//void operator()(const ParameterVectorSet & psg, ParameterVectorSet & psc, std::vector<scalar> shift=std::vector<scalar>(), bool append=false) const override {
-//  std::vector<scalar> psck(n);
-//  std::vector<scalar> psgk(n);
-//  for (size_t k=0; k<psc.size(); k++) {
-//      psg[k]->get(&psgk[0],n,0);
-//      if (append) {
-//          psc[k]->get(&psck[0],n,0);
-//          for (size_t i=0; i<n; i++)
-//            psck[i] -= psgk[i]/(shift[k]+2*i+alpha*(i+1));
-//        } else {
-//          for (size_t i=0; i<n; i++)
-//            psck[i] =- psgk[i]/(shift[k]+2*i+alpha*(i+1));
-//        }
-//      psc[k]->put(&psck[0],n,0);
-//    }
-//}
-//} _matrix_preconditioner;
+void update(LinearAlgebra::vectorSet<scalar> & psc, const LinearAlgebra::vectorSet<scalar> & psg, std::vector<scalar> shift=std::vector<scalar>()) {
+ std::vector<scalar> psck(n);
+ std::vector<scalar> psgk(n);
+ for (size_t k=0; k<psc.size(); k++) {
+  psg[k]->get(&psgk[0],n,0);
+  for (size_t i=0; i<n; i++)
+   psck[i] =- psgk[i]/(shift[k]+2*i+alpha*(i+1));
+  psc[k]->put(&psck[0],n,0);
+ }
+}
 
 int main(int argc, char *argv[])
 {
-//  alpha=100;
-//  n=10;
-//  Davidson solver();
-//  solver.m_verbosity=1;
-//  solver.m_roots=2;
-//  vectorSet<double> g;
-//  vectorSet<double> x;
-//  for (size_t root=0; root<solver.m_roots; root++) {
-//     x.push_back(std::make_shared<pv>(n));
-//     g.push_back(std::make_shared<pv>(n));
-//     x.back()->zero(); scalar one=1; x.back()->put(&one,1,root); // initial guess
-//    }
-//  if (not solver.solve(g,x)) std::cout << "Failure"<<std::endl;
-//  std::cout << "Error=";
-//  for (size_t root=0; root<solver.m_roots; root++)
-//    std::cout <<solver.errors()[root]<<" ";
-//  std::cout <<"after "<<solver.iterations()<<" iterations"<<std::endl;
-//  for (size_t root=0; root<solver.m_roots; root++) {
-//      std::vector<scalar> buf(n); x[root]->get(&buf[0],n,0);
-//      std::cout << "Eigenvector:"; for (size_t k=0; k<n; k++) std::cout<<" "<<buf[k]; std::cout<<std::endl;
-//    }
+  LinearAlgebra::LinearEigensystem<scalar> solver;
+  solver.m_verbosity=1;
+  solver.m_roots=4;
+  solver.m_thresh=1e-6;
+  LinearAlgebra::vectorSet<double> g;
+  LinearAlgebra::vectorSet<double> x;
+  for (int root=0; root<solver.m_roots; root++) {
+     x.push_back(std::make_shared<pv>(n));
+     g.push_back(std::make_shared<pv>(n));
+     x.back()->zero(); scalar one=1; x.back()->put(&one,1,root); // initial guess
+    }
+  for (auto iter=0; iter<1000; iter++) {
+   action(x,g);
+   solver.addVector(x,g);
+   update(x,g,solver.eigenvalues());
+   if (solver.endIteration(x,g)) break;
+  }
+  std::cout << "Error={ ";
+  for (int root=0; root<solver.m_roots; root++)
+    std::cout <<solver.errors()[root]<<" ";
+  std::cout <<"} after "<<solver.iterations()<<" iterations"<<std::endl;
+  for (int root=0; root<solver.m_roots; root++) {
+      std::vector<scalar> buf(n); x[root]->get(&buf[0],n,0);
+      std::cout << "Eigenvector:"; for (size_t k=0; k<n; k++) std::cout<<" "<<buf[k]; std::cout<<std::endl;
+    }
 }

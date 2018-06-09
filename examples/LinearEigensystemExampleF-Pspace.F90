20671a96d298a4cedab3512d3788b81a6a9f5675
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2018-06-09 15:11:11 +0100

    .

--- examples/LinearEigensystemExampleF-Pspace.F90
+++ examples/LinearEigensystemExampleF-Pspace.F90
@@ -24,6 +24,7 @@ PROGRAM LinearEigenSystemExample
 
  WRITE (6,*) 'P-space=',nP,', dimension=',n,', roots=',nroot
  CALL Iterative_Solver_Linear_Eigensystem_Initialize(n,nroot,thresh=1d-8,verbosity=1)
+ CALL Iterative_Solver_Option('convergence','residual') ! convergence threshold applies to norm of residual
  offsets(0)=0
  DO i=1,nP
   offsets(i)=i

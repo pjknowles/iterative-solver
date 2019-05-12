1d80d64a0cc784121664de196b90c263888e2b3a
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2019-05-12 21:27:27 +0100

    Tidy by removing Optimize::iterate()

--- lib/IterativeSolver.h
+++ lib/IterativeSolver.h
@@ -1364,50 +1364,6 @@ class Optimize : public Base<T> {
     this->m_singularity_threshold = 0;
   }
 
-  // this probably isn't needed
-//  /*!
-//   * @brief Introduce a new approximation to the optimum
-//   * @param parameters Current values of the parameter. On exit, contains new values predicted to be closer to optimum
-//   * @param gradient Derivative of the objective function with respect to parameters
-//   * @param preconditionedGradient gradient premultiplied by approximate inverse hessian
-//   * @param value Current value of the objective function
-//   * @return whether or not converged
-//   */
-//  bool iterate(T& parameters, T& gradient, const T& preconditionedGradient, const value_type value) {
-//    T parameterChanges(parameters);
-//    if (not this->m_solutions.empty()) parameterChanges.axpy(-1, this->m_solutions.back().back());
-//    this->copyvec(this->m_solutions, vectorRefSet(1, parameters));
-//    this->copyvec(m_parameterChanges, parameterChanges);
-//    this->copyvec(this->m_residuals, vectorRefSet(1, gradient));
-//    this->copyvec(m_preconditionedGradient, preconditionedGradient);
-//    size_t n = m_parameterChanges.size();
-//    this->m_iterations++;
-//
-//    m_ss.conservativeResize(n, n);
-//    for (size_t i = 0; i < n; i++)
-//      m_ss(i, n - 1) = m_ss(n - 1, i) = parameterChanges.dot(m_parameterChanges[i]);
-////    std::cout << "ss\n" << m_ss << std::endl;
-//
-//    m_gg.conservativeResize(n, n);
-//    size_t i = 0;
-//    for (const auto& g : this->m_residuals)
-//      for (const auto& gg : g) {
-//        m_gg(i, n - 1) = m_gg(n - 1, i) = gradient.dot(gg);
-//        ++i;
-//      }
-////    std::cout << "gg\n" << m_gg << std::endl;
-//
-//    if (m_algorithm == "null") {
-//      parameters.axpy(1, preconditionedGradient);
-//    } else
-//      throw std::logic_error(std::string{"Invalid algorithm "} + m_algorithm);
-//
-//    this->m_lastVectorIndex = n - 1;
-//    this->m_vector_active.push_back(std::vector<bool>(1, true));
-//    if (this->m_active.empty()) this->m_active.push_back(true);
-//    auto converged = this->endIteration(parameters, gradient);
-//    return converged;
-//  }
  protected:
   void solveReducedProblem() override {
     auto n = this->m_subspaceMatrix.rows();
@@ -1454,8 +1410,6 @@ class Optimize : public Base<T> {
  protected:
   std::string m_algorithm; ///< which variant of Quasi-Newton or other methods
   bool m_minimize; ///< whether to minimize or maximize
-  std::vector<T> m_parameterChanges;
-  std::vector<T> m_preconditionedGradient;
   Eigen::Matrix<scalar_type, Eigen::Dynamic, Eigen::Dynamic> m_gg;
   Eigen::Matrix<scalar_type, Eigen::Dynamic, Eigen::Dynamic> m_ss;
 
@@ -1621,14 +1575,14 @@ class DIIS : public Base<T> {
   }
 
   /*!
- * \brief Return the square L2 norm of the extrapolated residual from the last call to solveReducedProblem() or iterate().
+ * \brief Return the square L2 norm of the extrapolated residual from the last call to solveReducedProblem().
  * \return
  */
  public:
   scalar_type fLastResidual() const { return m_LastResidualNormSq; }
 
   /*!
- * \brief Return the coefficient of the last residual vector in the extrapolated residual from the last call to solveReducedProblem() or iterate().
+ * \brief Return the coefficient of the last residual vector in the extrapolated residual from the last call to solveReducedProblem().
  * \return
  */
   scalar_type fLastCoeff() const { return m_LastAmplitudeCoeff; }

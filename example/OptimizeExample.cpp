61e6e3d70168e0413a104c79328e4c709973b83d
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2019-05-23 10:08:44 +0100

    Restore Optimize example

--- example/OptimizeExample.cpp
+++ example/OptimizeExample.cpp
@@ -9,7 +9,7 @@ using pv = LinearAlgebra::PagedVector<scalar>;
 
 static double alpha;
 static double anharmonicity;
-static double n;
+static size_t n;
 
 scalar anharmonic_residual(const pv& psx, pv& outputs) {
   std::vector<scalar> psxk(n);
@@ -21,18 +21,6 @@ scalar anharmonic_residual(const pv& psx, pv& outputs) {
     output[i] = (alpha * (i + 1) + anharmonicity * (psxk[i] - 1)) * (psxk[i] - 1);
     for (size_t j = 0; j < n; j++)
       output[i] += (i + j) * (psxk[j] - 1);
-  }
-  outputs.put(output.data(), n, 0);
-  return value;
-}
-scalar trig_residual(const pv& psx, pv& outputs) {
-  std::vector<scalar> psxk(n);
-  std::vector<scalar> output(n);
-  psx.get(psxk.data(), n, 0);
-  scalar value = 0;
-  for (size_t i = 0; i < n; i++) {
-    value += 1-std::cos(scalar(i+1)*(psxk[i]-1));
-    output[i] =scalar(i+1)*std::sin(scalar(i+1)*(psxk[i]-1));
   }
   outputs.put(output.data(), n, 0);
   return value;
@@ -50,32 +38,28 @@ void update(pv& psc, const pv& psg) {
 
 int main(int argc, char* argv[]) {
   alpha = 7;
-  n = 1;
-  anharmonicity = 0.2;
+  n = 10;
+  anharmonicity = 0.7;
   for (const auto& method : std::vector<std::string>{
-    // "null",
+     "null",
     "L-BFGS"}) {
     std::cout << "optimize with " << method << std::endl;
     IterativeSolver::Optimize<pv> solver(std::regex_replace(method, std::regex("-iterate"), ""));
-    solver.m_verbosity = 2;
-    solver.m_maxIterations = 20;
-//    solver.m_Wolfe_1=.8;
-//    solver.m_linesearch_tolerance = .0001;
+    solver.m_maxIterations = 100;
+    solver.m_verbosity = 1;
     pv g(n);
     pv x(n);
     pv hg(n);
     scalar one = 1;
     for (auto i = 0; i < n; i++) x.put(&one, 1, i);
-    scalar zero = -0.58;
+    scalar zero = 0;
     x.put(&zero, 1, 0);  // initial guess
-    for (size_t iter = 0; iter < solver.m_maxIterations; ++iter) {
-      auto value = trig_residual(x, g);
-      xout << "iteration "<<iter<<" value="<<value<<"\n x: "<<x<<"\n g: "<<g<<std::endl;
+    for (size_t iter = 1; iter <= solver.m_maxIterations; ++iter) {
+      auto value = anharmonic_residual(x, g);
+//      xout << "iteration "<<iter<<" value="<<value<<"\n x: "<<x<<"\n g: "<<g<<std::endl;
       if (solver.addValue(x, value, g))
         update(x, g);
-      xout << "before endIteration\n x: "<<x<<"\n g: "<<g<<std::endl;
       if (solver.endIteration(x, g)) break;
-      xout << "after endIteration\n x: "<<x<<"\n g: "<<g<<std::endl;
     }
     for (size_t i = 0; i < x.size(); i++) {
       scalar val;

c1905f3e94f559898f3c5543ca9f872441a17ab4
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2019-09-18 18:08:26 +0100

    PagedVector-benchmark with barriers

--- benchmark/PagedVector-benchmark.cpp
+++ benchmark/PagedVector-benchmark.cpp
@@ -15,9 +15,11 @@ int main(int argc, char* argv[]) {
   using pv = LinearAlgebra::PagedVector<double, window_size>;
   for (int option = 0; option < 4; option++) {
     pv v0(vector_size,0);
+    MPI_Barrier(MPI_COMM_COMPUTE);
     auto startTiming = std::chrono::steady_clock::now();
     auto v1 = pv(v0, option);
     auto endTiming = std::chrono::steady_clock::now();
+    MPI_Barrier(MPI_COMM_COMPUTE);
     if (mpiRank==0)
     std::cout << " copy PagedVector from memory, construction option="<<option<<":  seconds="
               << std::chrono::duration_cast<std::chrono::nanoseconds>(endTiming - startTiming).count() * 1e-9

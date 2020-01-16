pjknowles/iterative-solver

commit ceececf7e638b8436e3b8c7f564a0027570c2cef

Fix eigensolver for non-hermitian problem

lib/IterativeSolver.h
@@ -927,8 +927,8 @@ class Base {
     history.back().reserve(newvec.size());
     for (auto& v : newvec)
       history.back().emplace_back(v,
-                           LINEARALGEBRA_DISTRIBUTED
-                               | LINEARALGEBRA_OFFLINE // TODO template-ise these options
+                                  LINEARALGEBRA_DISTRIBUTED
+                                      | LINEARALGEBRA_OFFLINE // TODO template-ise these options
       );
     if (!subtract.empty())
       for (size_t k = 0; k < newvec.size(); k++)
@@ -972,7 +972,7 @@ class Base {
 //        xout << "m_solutions: "<<m_solutions.size()<<m_solutions.back().front()<<std::endl;
         copyvec(m_others, other, m_last_other);
         m_subspaceGradient.conservativeResize(m_solutions.size(), residual.size());
-        size_t i=0;
+        size_t i = 0;
         for (size_t ii = 0; ii < m_solutions.size(); ii++) {
           if (not m_vector_active[ii][0]) continue;
           for (size_t k = 0; k < residual.size(); k++) {
@@ -1041,7 +1041,12 @@ class Base {
 //              xout << "bra"<<std::endl<<(*bra)[ll][lll]<<std::endl;
 //              xout << "residual1"<<std::endl<<residual1[kkk]<<std::endl;
 //              xout << "m_solutions[ll]"<<std::endl<<m_solutions[ll][lll]<<std::endl;
-              m_QQMatrix(k, l) = m_QQMatrix(l, k) = (*bra)[ll][lll].dot(residual1[kkk]);
+              if (m_hermitian) {
+                m_QQMatrix(k, l) = m_QQMatrix(l, k) = (*bra)[ll][lll].dot(residual1[kkk]);
+              } else {
+                m_QQMatrix(l, k) = (*bra)[ll][lll].dot(residual1[kkk]);
+                m_QQMatrix(k, l) = m_residuals[ll][lll].dot(solution1[kkk]);
+              }
               m_QQOverlap(k, l) = m_QQOverlap(l, k) = m_solutions[ll][lll].dot(solution1[kkk]);
               l++;
             }
@@ -1443,7 +1448,8 @@ class Optimize : public Base<T> {
   scalar_type m_Wolfe_1; ///< Acceptance parameter for function value
   scalar_type m_Wolfe_2; ///< Acceptance parameter for function gradient
   scalar_type m_linesearch_tolerance; ///< If the predicted line search is within tolerance of 1, don't bother taking it
-  scalar_type m_linesearch_grow_factor; ///< If the predicted line search step is extrapolation, limit the step to this factor times the current step
+  scalar_type
+      m_linesearch_grow_factor; ///< If the predicted line search step is extrapolation, limit the step to this factor times the current step
  protected:
   std::vector<scalar_type> m_linesearch_steps;
   std::vector<scalar_type> m_linesearch_values;

test/TestIterativeSolver.cpp
@@ -125,6 +125,113 @@ TEST(TestIterativeSolver, small_eigenproblem) {
   }
 }
 
+TEST(TestIterativeSolver, small_nonhermitian_eigenproblem) {
+  for (
+      size_t n = 2;
+      n < 90; n++) {
+    for (
+        size_t nroot = 1;
+        nroot <=
+            n && nroot <
+            10; nroot++) {
+      Eigen::MatrixXd m(n, n);
+      for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++)
+          m(i, j) = 2 * i + j + 3;
+      }
+      Eigen::EigenSolver<Eigen::MatrixXd> denseSolver(m);
+      auto val = denseSolver.eigenvalues();
+
+      LinearAlgebra::SimpleVector<double> mm(n);
+      std::vector<LinearAlgebra::SimpleVector<double> > x, g;
+      IterativeSolver::LinearEigensystem<LinearAlgebra::SimpleVector<double> > solver;
+      solver.m_verbosity = -1;
+      solver.setThresholds(1e-13);
+      if (solver.m_verbosity > 0)
+        std::cout << "Test n=" << n << ", nroot=" << nroot <<
+                  std::endl;
+      for (size_t root = 0; root < nroot; root++) {
+        x.emplace_back(n);
+        x.back().scal(0);
+        x.back()[root] = 1;
+        g.emplace_back(n);
+      }
+      for (size_t iter = 0; iter < n + 1; iter++) {
+        for (size_t root = 0; root < x.size(); root++) {
+          g[root].scal(0);
+          if (solver.active()[root])
+            for (size_t i = 0; i < n; i++)
+              for (size_t j = 0; j < n; j++)
+                g[root][j] += m(j, i) * x[root][i];
+        }
+
+//        std::cout << "eigenvector "<<0<<active[0]<<" before addVector"; for (size_t i = 0; i < n; i++) std::cout << " " << x[0][i]; std::cout << std::endl;
+        solver.addVector(x, g
+        );
+        for (size_t root = 0; root < x.size(); root++) {
+          if (solver.m_verbosity > 1) {
+            std::cout << "eigenvector " << root << " before update";
+            for (size_t i = 0; i < n; i++)
+              std::cout << " " << x[root][i];
+            std::cout << std::endl;
+          }
+          if (solver.active()[root]) {
+            for (size_t i = 0; i < n; i++)
+              x[root][i] -= g[root][i] / (m(i, i) - solver.eigenvalues()[root] + 1e-13);
+            if (solver.m_verbosity > 2) {
+              std::cout << "residual " << root << " ";
+              for (size_t i = 0; i < n; i++)
+                std::cout << " " << g[root][i];
+              std::cout << std::endl;
+            }
+            if (solver.m_verbosity > 1) {
+              std::cout << "eigenvector " << root << " ";
+              for (size_t i = 0; i < n; i++)
+                std::cout << " " << x[root][i];
+              std::cout << std::endl;
+            }
+          }
+        }
+//        std::cout << "eigenvector "<<0<<active[0]<<" before endIteration"; for (size_t i = 0; i < n; i++) std::cout << " " << x[0][i]; std::cout << std::endl;
+//        auto conv = (solver.endIteration(x, g, active));
+//        std::cout << "eigenvector "<<0<<active[0]<<" after endIteration"; for (size_t i = 0; i < n; i++) std::cout << " " << x[0][i]; std::cout << std::endl;
+//        if (conv) break;
+        if (solver.endIteration(x, g))
+          break;
+      }
+//  std::cout << "Error={ "; for (const auto& e : solver.errors()) std::cout << e << " "; std::cout << "} after " << solver.iterations() << " iterations" << std::endl;
+//  std::cout << "Actual eigenvalues\n"<<val<<std::endl;
+//      EXPECT_THAT(active,
+//                  ::testing::Pointwise(::testing::Eq(), std::vector<bool>(nroot, false)));
+      EXPECT_THAT(solver.errors(),
+                  ::testing::Pointwise(::testing::DoubleNear(1e-10), std::vector<double>(nroot, double(0))));
+//      EXPECT_THAT(solver.eigenvalues(),
+//                  ::testing::Pointwise(::testing::DoubleNear(1e-10),
+//                                       std::vector<double>(val.real().data(), val.real().data() + nroot)));
+      for (size_t root = 0; root < solver.m_roots; root++) {
+        if (solver.m_verbosity > 1) {
+          std::cout << "eigenvector " << root << " active=" << solver.active()[root] << " converged="
+                    << solver.errors()[root] << ":";
+          for (size_t i = 0; i < n; i++)
+            std::cout << " " << x[root][i];
+          std::cout << std::endl;
+        }
+        std::vector<double> r(n);
+        g[root].get(r.data(), n, 0);
+        EXPECT_THAT(r, ::testing::Pointwise(::testing::DoubleNear(1e-5), std::vector<double>(n, double(0))));
+        if (solver.m_verbosity > 1)
+          for (size_t soot = 0; soot <= root; soot++)
+            std::cout << "Eigenvector overlap " << root << " " << soot << " " << x[root].dot(x[soot]) << std::endl;
+        for (size_t soot = 0; soot < root; soot++)
+          EXPECT_LE (std::abs(x[root].dot(x[soot])),
+                     1e-8); // can't expect exact orthogonality when last thing might have been an update
+        EXPECT_THAT (std::abs(x[root].dot(x[root])), ::testing::DoubleNear(1, 1e-10));
+      }
+    }
+
+  }
+}
+
 TEST(TestIterativeSolver, linear_equations
 ) {
   for (size_t n = 1; n < 20; n++) {
@@ -505,7 +612,7 @@ class trigTest {
       bool upd = solver.addValue(x, value, g);
       if (!x.synchronised()) x.sync();
       if (!g.synchronised()) g.sync();
-      if (upd) update(x, g); 
+      if (upd) update(x, g);
       //if (solver.endIteration(x, g)) break;
       upd = solver.endIteration(x, g);
       if (!x.synchronised()) x.sync();
@@ -599,7 +706,7 @@ class optTest {
       bool upd = solver.addValue(x, value, g);
       if (!x.synchronised()) x.sync();
       if (!g.synchronised()) g.sync();
-      if (upd) update(x, g); 
+      if (upd) update(x, g);
       //if (solver.endIteration(x, g)) break;
       upd = solver.endIteration(x, g);
       if (!x.synchronised()) x.sync();

commit 1b8b2082fd63c682a73ee52089ad6084636432c3

Fix regression caused by ceececf7e638b8436e3b8c7f564a0027570c2cef

lib/IterativeSolver.h
@@ -1041,7 +1041,7 @@ class Base {
 //              xout << "bra"<<std::endl<<(*bra)[ll][lll]<<std::endl;
 //              xout << "residual1"<<std::endl<<residual1[kkk]<<std::endl;
 //              xout << "m_solutions[ll]"<<std::endl<<m_solutions[ll][lll]<<std::endl;
-              if (m_hermitian) {
+              if (m_hermitian or m_subspaceMatrixResRes) {
                 m_QQMatrix(k, l) = m_QQMatrix(l, k) = (*bra)[ll][lll].dot(residual1[kkk]);
               } else {
                 m_QQMatrix(l, k) = (*bra)[ll][lll].dot(residual1[kkk]);
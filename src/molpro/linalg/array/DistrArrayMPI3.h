45bf145ed18c73bdcb105872e80c50a151ac481b
Marat Sibaev <SibaevM@cardiff.ac.uk>
2020-07-08 10:23:14 +0100

    implemented LockMPI3

--- src/molpro/linalg/array/DistrArrayMPI3.h
+++ src/molpro/linalg/array/DistrArrayMPI3.h
@@ -21,7 +21,7 @@ class Distribution;
 class DistrArrayMPI3 : public DistrArray {
 protected:
 protected:
-  MPI_Win m_win; //!< window object
+  MPI_Win m_win = MPI_WIN_NULL; //!< window object
   //! distribution of array buffer among processes. Stores start index and size for each
   std::unique_ptr<util::Distribution> m_distribution;
   bool m_allocated = false; //!< whether the window has been created
@@ -76,6 +76,50 @@ protected:
                        RMAType option);
 };
 
+namespace util {
+//! Atomic lock allowing only one process to acquire it. Implemented using MPI3 RMA.
+class LockMPI3 {
+protected:
+  MPI_Comm m_comm = MPI_COMM_NULL; //! MPI communicator
+  MPI_Win m_win = MPI_WIN_NULL;    //! empty window handle
+  bool m_locked = false;           //!
+
+public:
+  //! Create the lock without acquiring it. This is collective and must be called by all processes in the communicator.
+  LockMPI3(MPI_Comm comm);
+  //! Release the lock and destroy it. This is collective and must be called by all processes in the communicator.
+  ~LockMPI3();
+
+  LockMPI3() = delete;
+  LockMPI3(const LockMPI3 &) = delete;
+  LockMPI3 &operator=(const LockMPI3 &) = delete;
+
+  //! Acquire exclusive lock
+  void lock();
+
+  //! Release the lock
+  void unlock();
+
+protected:
+  //! Proxy that locks on creation and unlocks on destruction. Useful for locking a scope.
+  struct Proxy {
+    LockMPI3 &m_lock;
+    Proxy(LockMPI3 &);
+    Proxy() = delete;
+    ~Proxy();
+
+    bool m_deleted = false; //!< whether the lock was already deleted
+  };
+
+  //! Keep track of proxy object so that of lock is deleted, the proxy does not try to unlock.
+  std::weak_ptr<Proxy> m_proxy;
+
+public:
+  //! Return a proxy object that acquires the lock on creation and releases it on destruction. Useful for locking a
+  //! scope.
+  std::shared_ptr<Proxy> scope();
+};
+} // namespace util
 } // namespace array
 } // namespace gci
 } // namespace molpro

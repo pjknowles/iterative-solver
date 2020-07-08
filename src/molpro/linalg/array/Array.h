377b959f441294f5db02af9deaa1e9c3ed88b99e
Marat Sibaev <sibaev.marat@gmail.com>
2020-07-08 10:23:14 +0100

    added Array.h with typedef to Array implementation, and options to choose the type

---
+++ src/molpro/linalg/array/Array.h
@@ -0,0 +1,19 @@
+#ifndef GCI_SRC_MOLPRO_GCI_ARRAY_ARRAY_H
+#define GCI_SRC_MOLPRO_GCI_ARRAY_ARRAY_H
+
+#ifdef GCI_ARRAY_ArrayGA
+#define GCI_ARRAY_ARRAY_TYPE ArrayGA
+#include "molpro/gci/array/ArrayGA.h"
+#elif GCI_ARRAY_ArrayMPI3
+#define GCI_ARRAY_ARRAY_TYPE ArrayMPI3
+#include "molpro/gci/array/ArrayMPI3.h"
+#else
+#define GCI_ARRAY_ARRAY_TYPE ArrayMPI3
+#include "molpro/gci/array/ArrayMPI3.h"
+#endif
+
+namespace molpro::gci::array {
+using Array = GCI_ARRAY_ARRAY_TYPE;
+}
+#undef GCI_ARRAY_ARRAY_TYPE
+#endif // GCI_SRC_MOLPRO_GCI_ARRAY_ARRAY_H

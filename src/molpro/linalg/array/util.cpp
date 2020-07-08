1e67d0616e9223be25c4b723406a95bb66171b07
Marat Sibaev <SibaevM@cardiff.ac.uk>
2020-07-08 10:23:14 +0100

    tested util::Distribution

--- src/molpro/linalg/array/util.cpp
+++ src/molpro/linalg/array/util.cpp
@@ -19,21 +19,22 @@ Distribution::Distribution(int n_proc, size_t dimension) : dim(dimension) {
   auto block = dim / n_proc;
   // Spread the remainder over the first processes
   auto n_extra = dim % n_proc;
-  for (size_t i = 0; i < n_extra; ++i)
-    proc_buffer.emplace_back(i * (block + 1), block + 1);
-  for (size_t i = n_extra; i < n_proc; ++i)
-    proc_buffer.emplace_back(i * block, block);
+  size_t lo = 0;
+  for (size_t i = 0; i < n_extra; ++i, lo += block + 1)
+    proc_buffer.emplace_back(lo, block + 1);
+  for (size_t i = n_extra; i < n_proc; ++i, lo += block)
+    proc_buffer.emplace_back(lo, block);
 }
 
 std::pair<int, int> Distribution::process_map(unsigned long lo, unsigned long hi) {
   auto block = dim / proc_buffer.size();
   auto n_extra = dim % proc_buffer.size();
   auto size_large_chunk = (block + 1) * n_extra;
-  auto find_process = [size_large_chunk, block](auto ind) -> int {
+  auto find_process = [size_large_chunk, n_extra, block](auto ind) -> int {
     if (ind < size_large_chunk)
       return (ind / (block + 1));
     else
-      return ((ind - size_large_chunk) / (block));
+      return (n_extra + (ind - size_large_chunk) / (block));
   };
   return {find_process(lo), find_process(hi)};
 }

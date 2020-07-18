563e444e256b0e7ffc90f8f62a256d75e30e9ba4
Marat Sibaev <sibaev.marat@gmail.com>
2020-07-18 19:56:27 +0100

    updated docs and filled in the header

--- src/molpro/linalg/array/DistrArrayDisk.h
+++ src/molpro/linalg/array/DistrArrayDisk.h
@@ -2,18 +2,21 @@
 #define LINEARALGEBRA_SRC_MOLPRO_LINALG_ARRAY_DISTRARRAYDISK_H
 
 #include "molpro/linalg/array/DistrArray.h"
+#include "molpro/linalg/array/Span.h"
 
 namespace molpro {
 namespace linalg {
 namespace array {
 namespace util {
-template <typename Result> class Task;
-}
+template <typename Result>
+class Task;
+} // namespace util
 /*!
  * @brief Distributed array located primarily on disk
  *
- * This class stores the full array on disk. Sections of the array can still be obtained by reading into a buffer.
- * The local section of array  can still be accessed by the LocalBuffer.
+ * This class stores the full array on disk. RMA operations read data from disk. A memory view buffer can be allocated
+ * which will read the local buffer from disk and keep array in memory until it is flushed. This allows multiple
+ * operations on local section to be performed without I/O at the end of each one.
  *
  * Concepts
  * --------
@@ -29,24 +32,16 @@ template <typename Result> class Task;
  *
  * allocate_buffer() creates the memory view buffer. The buffer will persist until deallocate_buffer() is called.
  *
- * LocalBuffer will use the memory view buffer if it was allocated and not in use by a different instance of
- * LocalBuffer. Otherwise, it will allocate a new buffer which will be released on its destruction. Note that the new
- * buffer will be a snapshot of the array currently on disk. If memory view was not flushed after modification than the
- * snapshot will be out of sync. LocalBuffer::memory_view() returns true if an instance is using the memory view buffer
- * or false if it is a snapshot from disk. A non-const local buffer snapshot is always written to disk on destruction,
- * even if it was not modified. Prefer to use get() instead of LocalBuffer for read only access.
- *
- * Linear algebra operations are done with a memory view if it is allocated and valid, otherwise a snapshot.
+ * LocalBuffer will use the memory view buffer if it was allocated, otherwise it will allocate a new buffer. The new
+ * buffer will be a snapshot of the array currently on disk. A non-const local buffer snapshot is always written to disk
+ * on destruction, even if it was not modified.
  *
  * Linear algebra operations work by creating a LocalBuffer instance which loads relevant section of the array,
- * followed by numerical operations and optionally dumping of LocalBuffer to disk if it was modified. If there are
- * multiple overlapping calls to linear algebra operations than distinct LocalBuffer copies will be created, which might
- * lead to large memory usage as well as undefined behaviour if array on disk gets overwritten with results.
+ * followed by numerical operations and optionally dumping of LocalBuffer to disk if it was modified.
  *
- * To minimize I/O the allocated buffer is reused as long as it is in sync with the disc version. Operations such as
- * writing to a non-local section of an array invalidates the local buffer on processes that own it. Similarly, any
- * LocalBuffer that is a snap shot is out of sync, if it writes to disk than the memory view buffer is invalidated. When
- * the memory view buffer is invalidated, it will be read from disk again on next usage.
+ * To minimize I/O the memory view buffer is reused as long as it is in sync with the disc version. Operations such as
+ * writing to a non-local section of an array invalidates the local buffer on processes that own it. When the memory
+ * view buffer is invalidated, it will be read from disk again on next usage.
  *
  * Flushing writes the memory view to disk if it was allocated and is valid.
  *
@@ -55,7 +50,7 @@ template <typename Result> class Task;
  * for each process).
  *
  * IO can be done with a thread
- * ------------------------
+ * ----------------------------
  *
  * Some RMA operations have a threaded implementation their names are prefixed with "t", e.g tput(), tget(). The
  * underlying IO operation is done in a separate thread allowing user to overlap it with computation. The separate
@@ -70,20 +65,12 @@ template <typename Result> class Task;
  * Advice
  * ------
  *
- * **To minimize RAM usage**, call allocate_buffer() and deallocate_buffer() between a section of computation with
- * many RMA or linear algebra calls. Try not to use multiple LocalBuffer instances at the same time
- *
- * **To minimize IO**, avoid using multiple LocalBuffer instances. Only one instance can use the memory view buffer,
- * with the others creating snapshots of the disk array.
- *
- * If there are sequential linear algebra operations
- * that modify the local buffer, prefer to use an ArrayHandler to fuse operations via lazy evaluator, or implement
- * operations manually using LocalBuffer and dumping it only at the end.
+ * Call allocate_buffer() and deallocate_buffer() between a section of computation with many linear algebra calls.
+ * Operations will be done using memory view buffer without any IO.  At the end a flush() or sync() call has to be made
+ * to write the new array to disk.
  *
  * Mixing up incompatible RMA and linear algebra operations can lead to undefined behavior of DistrArray. This is also
  * true for DistrArrayDisk and the buffering mechanism where the allocated buffer mimics data stored on disk.
- *
- * @note Linear algebra operations are done with LocalBuffer instances.
  *
  * @code{.cpp}
  * auto da = DistrArrayDisk{...};
@@ -97,23 +84,59 @@ template <typename Result> class Task;
  */
 class DistrArrayDisk : public DistrArray {
 protected:
-  bool m_invalid; //!< Flags that the allocated buffer is out of sync with the array on disk
+  bool m_allocated;          //!< Flags that the memory view buffer has been allocated
+  bool m_invalid;            //!< Flags that the allocated buffer is out of sync with the array on disk
+  Span<value_type> m_buffer; //!< memory view buffer either wraps allocated buffer or stores user supplied buffer
+  std::vector<value_type> m_allocated_buffer; //!< buffer allocated by the class
+  explicit DistrArrayDisk(const DistrArrayDisk &source) = default;
+
 public:
-  explicit DistrArrayDisk(const DistrArrayDisk& source) = delete;
   //! Opens access to the storage on disk, creating the underlying storage object if it does not exist. Assume
   //! collective, but not all implementations might require that.
   virtual void open_access() = 0;
   //! Close access to the storage on disk, buffer is flushed to disk. Assume collective, but not all implementations
   //! might require that.
   virtual void close_access() = 0;
+  void allocate_buffer() override;
+  //! Uses provided buffer. @returns false if allocation fails because a buffer was already allocated
+  bool allocate_buffer(Span<value_type> buffer);
   //! Release the allocated buffer
-  virtual void deallocate_buffer() = 0;
+  virtual void deallocate_buffer();
   //! Writes memory view buffer
-  virtual void flush() = 0;
+  virtual void flush();
+  void sync() const override;
+  bool compatible(const DistrArrayDisk &other) const;
   //! Erase the array from disk deleting the underlying storage object.
   virtual bool erase() = 0;
   //! Returns true if the allocated buffer is out of sync with the array on disk
   bool invalid() const { return m_invalid; };
+  //! Marks memory view buffer out of sync
+  void invalidate() { m_invalid = true; };
+
+protected:
+  class LocalBufferDisk : public DistrArray::LocalBuffer {
+  public:
+    LocalBufferDisk(DistrArrayDisk &source);
+
+  protected:
+    bool m_dump_on_destruct = true; //!< whether to dump contents of local buffer to disk on destruction
+  };
+
+public:
+  [[nodiscard]] std::unique_ptr<LocalBuffer> local_buffer() override;
+  [[nodiscard]] std::unique_ptr<const LocalBuffer> local_buffer() const override;
+  [[nodiscard]] const Distribution &distribution() const override;
+
+  value_type at(index_type ind) const override;
+  void set(index_type ind, value_type val) override;
+  std::vector<value_type> get(index_type lo, index_type hi) const override;
+  void get(index_type lo, index_type hi, value_type *buf) const override;
+  void put(index_type lo, index_type hi, const value_type *data) override;
+  void acc(index_type lo, index_type hi, const value_type *data) override;
+  std::vector<value_type> gather(const std::vector<index_type> &indices) const override;
+  void scatter(const std::vector<index_type> &indices, const std::vector<value_type> &data) override;
+  void scatter_acc(std::vector<index_type> &indices, const std::vector<value_type> &data) override;
+  std::vector<value_type> vec() const override;
 
   //! Writes section of array to disk in a separate thread, bypassing the memory view.
   //! @return Task manager that can test and/or wait for completion

a5c219eb3512d620b5f4a4043438788df98461c3
Marat Sibaev <sibaev.marat@gmail.com>
2020-07-18 21:56:54 +0100

    implementing DistrArrayDisk

--- src/molpro/linalg/array/DistrArrayDisk.cpp
+++ src/molpro/linalg/array/DistrArrayDisk.cpp
@@ -1,5 +1,132 @@
-//
-// Created by marat on 14/07/2020.
-//
-
 #include "DistrArrayDisk.h"
+namespace molpro {
+namespace linalg {
+namespace array {
+namespace {
+
+int mpi_rank(MPI_Comm comm) {
+  int rank;
+  MPI_Comm_rank(comm, &rank);
+  return rank;
+}
+
+} // namespace
+
+DistrArrayDisk::DistrArrayDisk(size_t dimension, MPI_Comm commun, std::shared_ptr<Profiler> prof)
+    : DistrArray(dimension, commun, std::move(prof)) {}
+
+DistrArrayDisk::LocalBufferDisk::LocalBufferDisk(DistrArrayDisk& source) : m_source{source} {
+  if (!source.m_allocated) {
+    m_start = m_size = 0;
+    m_buffer = nullptr;
+    return;
+  }
+  int rank;
+  MPI_Comm_rank(source.communicator(), &rank);
+  std::tie(m_start, m_size) = source.distribution().range(rank);
+  bool get_data = false;
+  if (!source.m_allocated) {
+    m_snapshot_buffer.resize(m_size);
+    m_buffer = m_snapshot_buffer.data();
+    m_dump = true;
+    get_data = true;
+  } else {
+    m_buffer = source.m_buffer.data();
+    m_dump = false;
+    get_data = !source.is_valid();
+  }
+  if (get_data) {
+    MPI_Win_lock(MPI_LOCK_EXCLUSIVE, rank, 0, source.m_win);
+    source.get(start(), start() + size() - 1, m_buffer);
+    MPI_Win_unlock(rank, source.m_win);
+  }
+}
+
+bool DistrArrayDisk::LocalBufferDisk::memory_view() { return m_snapshot_buffer.empty(); }
+
+DistrArrayDisk::LocalBufferDisk::~LocalBufferDisk() {
+  if (m_dump)
+    m_source.put(start(), start() + size() - 1, m_buffer);
+}
+
+void DistrArrayDisk::allocate_buffer() {
+  if (m_allocated)
+    return;
+  if (m_win == MPI_WIN_NULL) {
+    int* base = nullptr;
+    int size_of_type = sizeof(int);
+    MPI_Win_allocate(size_of_type, size_of_type, MPI_INFO_NULL, m_communicator, &base, &m_win);
+  }
+  int rank;
+  MPI_Comm_rank(communicator(), &rank);
+  index_type _lo;
+  size_t sz;
+  std::tie(_lo, sz) = distribution().range(rank);
+  if (m_allocated_buffer.size() < sz) {
+    m_allocated_buffer.resize(sz);
+    mark_validity(false);
+  }
+  m_buffer = Span<value_type>(m_allocated_buffer.data(), m_allocated_buffer.size());
+  m_allocated = true;
+}
+
+void DistrArrayDisk::allocate_buffer(Span<value_type> buffer) {
+  int rank;
+  MPI_Comm_rank(communicator(), &rank);
+  index_type _lo;
+  size_t sz;
+  std::tie(_lo, sz) = distribution().range(rank);
+  if (buffer.size() < sz)
+    error("provided buffer is too small");
+  deallocate_buffer();
+  m_buffer = buffer;
+  m_allocated = true;
+  mark_validity(false);
+}
+
+void DistrArrayDisk::deallocate_buffer() {
+  m_buffer = Span<value_type>{};
+  m_allocated_buffer.clear();
+  m_allocated_buffer.shrink_to_fit();
+  m_allocated = false;
+  mark_validity(false);
+}
+
+void DistrArrayDisk::mark_validity(bool valid) {
+  if (m_win == MPI_WIN_NULL)
+    return;
+  int buffer = valid ? 1 : 0;
+  auto rank = mpi_rank(communicator());
+  MPI_Win_lock(MPI_LOCK_EXCLUSIVE, rank, 0, m_win);
+  MPI_Put(&buffer, 1, MPI_INT, rank, 0, 1, MPI_INT, m_win);
+  MPI_Win_unlock(rank, m_win);
+}
+bool DistrArrayDisk::is_valid() {
+  if (m_win == MPI_WIN_NULL)
+    return false;
+  int buffer = 0;
+  auto rank = mpi_rank(communicator());
+  MPI_Win_lock(MPI_LOCK_EXCLUSIVE, rank, 0, m_win);
+  MPI_Get(&buffer, 1, MPI_INT, rank, 0, 1, MPI_INT, m_win);
+  MPI_Win_unlock(rank, m_win);
+  return buffer;
+}
+
+void DistrArrayDisk::flush() {
+  if (!m_allocated)
+    return;
+  auto rank = mpi_rank(communicator());
+  index_type _lo;
+  size_t sz;
+  std::tie(_lo, sz) = distribution().range(rank);
+  if (is_valid())
+    put(_lo, _lo + sz - 1, m_buffer.data());
+  else
+    get(_lo, _lo + sz - 1, m_buffer.data());
+}
+
+bool DistrArrayDisk::empty() const { return m_allocated; }
+
+} // namespace array
+} // namespace linalg
+} // namespace molpro
--- src/molpro/linalg/array/DistrArrayDisk.h
+++ src/molpro/linalg/array/DistrArrayDisk.h
@@ -45,9 +45,8 @@ class Task;
  *
  * Flushing writes the memory view to disk if it was allocated and is valid.
  *
- * Synchronisation has a dual effect of synchronising the processes, and if the memory view was allocated synchronising
- * it with disk. If memory view is valid, than it is written to disk, otherwise it is read from disk (may be different
- * for each process).
+ * To keep memory view in sync the buffer has to be flushed. If memory view is valid, than it is written to disk,
+ * otherwise it is read from disk (may be different for each process).
  *
  * IO can be done with a thread
  * ----------------------------
@@ -84,11 +83,15 @@ class Task;
  */
 class DistrArrayDisk : public DistrArray {
 protected:
-  bool m_allocated;          //!< Flags that the memory view buffer has been allocated
-  bool m_invalid;            //!< Flags that the allocated buffer is out of sync with the array on disk
+  bool m_allocated = false;  //!< Flags that the memory view buffer has been allocated
   Span<value_type> m_buffer; //!< memory view buffer either wraps allocated buffer or stores user supplied buffer
   std::vector<value_type> m_allocated_buffer; //!< buffer allocated by the class
+  MPI_Win m_win = MPI_WIN_NULL;               //!< window for marking local process buffers as valid or invalid
+  DistrArrayDisk(size_t dimension, MPI_Comm commun, std::shared_ptr<Profiler> prof = nullptr);
   explicit DistrArrayDisk(const DistrArrayDisk &source) = default;
+
+  //! Marks memory view buffer as invalid
+  void mark_validity(bool valid);
 
 public:
   //! Opens access to the storage on disk, creating the underlying storage object if it does not exist. Assume
@@ -98,28 +101,39 @@ public:
   //! might require that.
   virtual void close_access() = 0;
   void allocate_buffer() override;
-  //! Uses provided buffer. @returns false if allocation fails because a buffer was already allocated
-  bool allocate_buffer(Span<value_type> buffer);
+  //! Uses a provided buffer even if another has been allocated. This might invalidate any active LocalBuffer instances
+  void allocate_buffer(Span<value_type> buffer);
+  bool empty() const override;
   //! Release the allocated buffer
-  virtual void deallocate_buffer();
+  void deallocate_buffer();
   //! Writes memory view buffer
   virtual void flush();
-  void sync() const override;
-  bool compatible(const DistrArrayDisk &other) const;
+  virtual bool compatible(const DistrArrayDisk &other) const = 0;
   //! Erase the array from disk deleting the underlying storage object.
   virtual bool erase() = 0;
   //! Returns true if the allocated buffer is out of sync with the array on disk
-  bool invalid() const { return m_invalid; };
+  bool is_valid();
   //! Marks memory view buffer out of sync
-  void invalidate() { m_invalid = true; };
+  void invalidate() { mark_validity(false); };
 
 protected:
   class LocalBufferDisk : public DistrArray::LocalBuffer {
   public:
     LocalBufferDisk(DistrArrayDisk &source);
+    ~LocalBufferDisk();
+    //! Prints true if the buffer is memory view from source array
+    bool memory_view();
+    //! turns off dumping of snapshot buffer to disk
+    void dump_off() { m_dump = false; };
+    //! turns on dumping of snapshot buffer to disk
+    void dump_on() { m_dump = true; };
+    //! whether dumping is on or off
+    bool dump() const { return m_dump; };
 
   protected:
-    bool m_dump_on_destruct = true; //!< whether to dump contents of local buffer to disk on destruction
+    bool m_dump = true;                        //! If true than snapshot buffer is dumped to disk on destruction
+    std::vector<value_type> m_snapshot_buffer; //!< buffer for storing a snap shot when memory view is not allocated
+    DistrArrayDisk &m_source;                  //!< keep a handle on source to dump data to disk
   };
 
 public:
@@ -140,19 +154,27 @@ public:
 
   //! Writes section of array to disk in a separate thread, bypassing the memory view.
   //! @return Task manager that can test and/or wait for completion
-  virtual std::unique_ptr<util::Task<void>> tput() = 0;
-  //!
+  virtual std::unique_ptr<util::Task<void>> tput(index_type lo, index_type hi, const value_type *data) = 0;
   /*!
    * @brief Reads section of array from disk in a separate thread
    * @warning Reading and writing bypasses the memory-view, the buffer should be flushed before
    * @return Task manager that can test and/or wait for completion
    */
-  virtual std::unique_ptr<util::Task<void>> tget() = 0;
+  virtual std::unique_ptr<util::Task<void>> tget(index_type lo, index_type hi, value_type *buf) = 0;
+  virtual std::unique_ptr<std::vector<value_type>> tget(index_type lo, index_type hi) = 0;
+  virtual std::unique_ptr<util::Task<value_type>> tat(index_type ind) const;
+  virtual std::unique_ptr<util::Task<void>> tset(index_type ind, value_type val);
+  virtual std::unique_ptr<util::Task<void>> tacc(index_type lo, index_type hi, const value_type *data);
+  virtual std::unique_ptr<util::Task<std::vector<value_type>>> tgather(const std::vector<index_type> &indices) const;
+  virtual std::unique_ptr<util::Task<void>> tscatter(const std::vector<index_type> &indices,
+                                                     const std::vector<value_type> &data);
+  virtual std::unique_ptr<util::Task<void>> tscatter_acc(std::vector<index_type> &indices,
+                                                         const std::vector<value_type> &data);
+  virtual std::unique_ptr<util::Task<std::vector<value_type>>> tvec() const;
 
-protected:
-public:
   //! Loads the local buffer in a separate thread
-  std::pair<LocalBuffer, std::unique_ptr<util::Task<void>>> tlocal_buffer();
+  std::unique_ptr<util::Task<std::shared_ptr<LocalBuffer>>> tlocal_buffer();
+  std::unique_ptr<util::Task<std::shared_ptr<const LocalBuffer>>> tlocal_buffer() const;
 };
 
 } // namespace array

dab6b47c92478ce5ad4c29dea700011d46d496e5
Marat Sibaev <sibaev.marat@gmail.com>
2020-08-07 10:45:58 +0100

    implemented ArrayHandlerDistrSparse for hanlding distributed and a sparse array

---
+++ src/molpro/linalg/array/ArrayHandlerDistrSparse.h
@@ -0,0 +1,49 @@
+#ifndef LINEARALGEBRA_SRC_MOLPRO_LINALG_ARRAY_ARRAYHANDLERITERABLESPARSE_H
+#define LINEARALGEBRA_SRC_MOLPRO_LINALG_ARRAY_ARRAYHANDLERITERABLESPARSE_H
+#include "molpro/linalg/array/ArrayHandler.h"
+#include <numeric>
+
+namespace molpro {
+namespace linalg {
+namespace array {
+template <typename AL, typename AR, bool = util::has_mapped_type<AR>{}>
+class ArrayHandlerDistrSparse : public ArrayHandler<AL, AR> {};
+
+/*!
+ * @brief Array handler between an iterable and a sparse arrays. Iterable container must implement access operator.
+ */
+template <typename AL, typename AR>
+class ArrayHandlerDistrSparse<AL, AR, true> : public ArrayHandler<AL, AR> {
+public:
+  using typename ArrayHandler<AL, AR>::value_type_L;
+  using typename ArrayHandler<AL, AR>::value_type_R;
+  using typename ArrayHandler<AL, AR>::value_type;
+  using typename ArrayHandler<AL, AR>::ProxyHandle;
+
+  AL copy(const AR &source) override {
+    static_assert(true, "General copy from sparse to dense is ill-defined");
+    return {};
+  };
+
+  void scal(value_type alpha, AL &x) override { static_assert(true, "Use ArrayHandlerDistr for unary operations"); };
+
+  void fill(value_type alpha, AL &x) override { static_assert(true, "Use ArrayHandlerDistr for unary operations"); };
+
+  // TODO For now these functions are in DistrArray, but they should be moved into the handler.
+  void axpy(value_type alpha, const AR &x, AL &y) override { y.axpy(alpha, x); };
+
+  value_type dot(const AL &x, const AR &y) override { return x.dot(y); };
+
+  ProxyHandle lazy_handle() override { return this->lazy_handle(*this); };
+
+protected:
+  using ArrayHandler<AL, AR>::error;
+  using ArrayHandler<AL, AR>::lazy_handle;
+  using ArrayHandler<AL, AR>::m_lazy_handles;
+}; // namespace linalg
+
+} // namespace array
+} // namespace linalg
+} // namespace molpro
+
+#endif // LINEARALGEBRA_SRC_MOLPRO_LINALG_ARRAY_ARRAYHANDLERITERABLESPARSE_H

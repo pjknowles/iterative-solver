58374b17226fd1ed8c2842b6c5e7d8a93acab308
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2020-08-18 14:28:51 +0100

    Tidy

--- benchmark/ArrayBenchmark.h
+++ benchmark/ArrayBenchmark.h
@@ -63,62 +63,59 @@ class ArrayBenchmark {
   int m_mpi_size;
 
 public:
+  size_t m_repeat;
   explicit ArrayBenchmark(std::string title, std::unique_ptr<array::ArrayHandler<L, R>> handler, size_t n = 10000000,
                           bool profile_individual = false, double target_seconds = 1)
       : m_size(n), m_target_seconds(target_seconds), m_bufferL(allocate<L>(n)), m_bufferR(allocate<R>(n)),
-        m_profiler(title), m_handler(std::move(handler)), m_profile_individual(profile_individual), m_mpi_size(1) {
+        m_profiler(title), m_handler(std::move(handler)), m_profile_individual(profile_individual), m_mpi_size(1),
+        m_repeat(std::max(1, int(1e9 * m_target_seconds / m_size))) {
 #ifdef HAVE_MPI_H
     MPI_Comm_size(MPI_COMM_WORLD, &m_mpi_size);
 #endif
   }
 
   void dot() {
-    size_t repeat = std::max(1, int(1e9 * m_target_seconds / m_size));
     auto prof = m_profiler.push("dot");
-    for (auto i = 0; i < repeat; i++)
+    for (auto i = 0; i < m_repeat; i++)
       m_handler->dot(*m_bufferL, *m_bufferR);
-    prof += m_size * repeat / m_mpi_size;
+    prof += m_size * m_repeat / m_mpi_size;
   }
 
   void axpy() {
-    size_t repeat = std::max(1, int(1e9 * m_target_seconds / m_size));
     auto prof = m_profiler.push("axpy");
-    for (auto i = 0; i < repeat; i++)
+    for (auto i = 0; i < m_repeat; i++)
       m_handler->axpy(1.0, *m_bufferL, *m_bufferR);
-    prof += m_size * repeat / m_mpi_size;
+    prof += m_size * m_repeat / m_mpi_size;
   }
 
   void copy() {
-    size_t repeat = std::max(1, int(1e9 * m_target_seconds / m_size));
     auto prof = m_profiler.push("copy");
-    for (auto i = 0; i < repeat / m_mpi_size; i++)
+    for (auto i = 0; i < m_repeat / m_mpi_size; i++)
       *m_bufferL = m_handler->copy(*m_bufferR);
-    prof += m_size * repeat / m_mpi_size;
+    prof += m_size * m_repeat / m_mpi_size;
   }
 
   void fill() {
     using scalar_type = typename L::value_type;
-    size_t repeat = std::max(1, int(1e9 * m_target_seconds / m_size));
     auto prof = m_profiler.push("fill");
-    for (auto i = 0; i < repeat; i++)
+    for (auto i = 0; i < m_repeat; i++)
       m_handler->fill(static_cast<scalar_type>(1), *m_bufferL);
-    prof += m_size * repeat / m_mpi_size;
+    prof += m_size * m_repeat / m_mpi_size;
   }
 
   void scal() {
     using scalar_type = typename L::value_type;
-    size_t repeat = std::max(1, int(1e9 * m_target_seconds / m_size));
     if (m_profile_individual)
-      for (auto i = 0; i < repeat; i++) {
+      for (auto i = 0; i < m_repeat; i++) {
         auto prof = m_profiler.push("scal");
         m_handler->scal(1, *m_bufferL);
         prof += m_size / m_mpi_size;
       }
     else {
       auto prof = m_profiler.push("scal");
-      for (auto i = 0; i < repeat; i++)
+      for (auto i = 0; i < m_repeat; i++)
         m_handler->scal(1, *m_bufferL);
-      prof += m_size * repeat / m_mpi_size;
+      prof += m_size * m_repeat / m_mpi_size;
     }
   }
 
@@ -140,22 +137,15 @@ std::ostream& operator<<(std::ostream& os, ArrayBenchmark<L, R>& obj) {
 }
 
 template <class L = std::vector<double>, class R = L>
-ArrayBenchmark<L, R> ArrayBenchmarkII(std::string title, size_t n = 10000000, bool profile_individual = false,
-                                      double target_seconds = 1) {
+ArrayBenchmark<L, R> ArrayBenchmarkIterable(std::string title, size_t n = 10000000, bool profile_individual = false,
+                                            double target_seconds = 1) {
   return ArrayBenchmark<L, R>(title, std::make_unique<array::ArrayHandlerIterable<L, R>>(), n, profile_individual,
                               target_seconds);
 }
 
-template <class L = std::vector<double>, class R = array::DistrArrayMPI3>
-ArrayBenchmark<L, R> ArrayBenchmarkID(std::string title, size_t n = 10000000, bool profile_individual = false,
-                                      double target_seconds = 1) {
-  return ArrayBenchmark<L, R>(title, std::make_unique<array::ArrayHandlerDistr<L, R>>(), n, profile_individual,
-                              target_seconds);
-}
-
 template <class L = array::DistrArrayMPI3, class R = L>
-ArrayBenchmark<L, R> ArrayBenchmarkDD(std::string title, size_t n = 10000000, bool profile_individual = false,
-                                      double target_seconds = 1) {
+ArrayBenchmark<L, R> ArrayBenchmarkDistributed(std::string title, size_t n = 10000000, bool profile_individual = false,
+                                               double target_seconds = 1) {
   return ArrayBenchmark<L, R>(title, std::make_unique<array::ArrayHandlerDistr<L, R>>(), n, profile_individual,
                               target_seconds);
 }
--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -15,42 +15,39 @@ int main(int argc, char* argv[]) {
 #ifdef LINEARALGEBRA_ARRAY_GA
   GA_Initialize();
 #endif
-  for (const auto& length : std::vector<size_t>{100000, 1000000, 10000000, 100000000}) {
+  for (const auto& length : std::vector<size_t>{500, 1000, 10000, 100000, 1000000, 10000000, 100000000}) {
 
-    std::cout << "Vector length " << length << std::endl;
-    molpro::linalg::ArrayBenchmarkII<std::vector<double>>("std::vector<double>", length, true, 1e-6)
-        .axpy(); // flush memory
+    std::cout << "Vector length = " << length;
+    {
+      auto bm = molpro::linalg::ArrayBenchmarkIterable<std::vector<double>>("std::vector<double>", length);
+      bm.axpy(); // flush memory
+      std::cout << ", repeat count = " << bm.m_repeat << std::endl;
+    }
     if (mpi_size <= 1) {
-      auto bm = molpro::linalg::ArrayBenchmarkII<std::vector<double>>("std::vector<double>", length);
+      auto bm = molpro::linalg::ArrayBenchmarkIterable<std::vector<double>>("std::vector<double>", length);
       bm.all();
       if (rank == 0)
         std::cout << bm;
     }
 #ifdef LINEARALGEBRA_ARRAY_MPI3
     {
-      auto bm = molpro::linalg::ArrayBenchmarkDD<molpro::linalg::array::DistrArrayMPI3>(
+      auto bm = molpro::linalg::ArrayBenchmarkDistributed<molpro::linalg::array::DistrArrayMPI3>(
           "molpro::linalg::array::DistrArrayMPI3", length);
       bm.all();
       std::cout << bm;
     }
-//    {
-//      auto bm = molpro::linalg::ArrayBenchmarkID<std::vector<double>, molpro::linalg::array::DistrArrayMPI3>(
-//          "std::vector<double>,molpro::linalg::array::DistrArrayMPI3", length);
-//      bm.all();
-//      std::cout << bm;
-//    }
 #endif
 #ifdef LINEARALGEBRA_ARRAY_GA
     {
-      auto bm = molpro::linalg::ArrayBenchmarkDD<molpro::linalg::array::DistrArrayGA>(
+      auto bm = molpro::linalg::ArrayBenchmarkDistributed<molpro::linalg::array::DistrArrayGA>(
           "molpro::linalg::array::DistrArrayGA", length);
       bm.all();
       std::cout << bm;
     }
 #endif
 #ifdef LINEARALGEBRA_ARRAY_HDF5
-    {
-      //    auto bm = molpro::linalg::ArrayBenchmarkDD<molpro::linalg::array::DistrArrayHDF5>(
+    { // TODO uncomment this when DistrArrayHDF5 is ready
+      //    auto bm = molpro::linalg::ArrayBenchmarkDistributed<molpro::linalg::array::DistrArrayHDF5>(
       //        "molpro::linalg::array::DistrArrayHDF5",length);
       //    bm.all();
       //    std::cout << bm ;

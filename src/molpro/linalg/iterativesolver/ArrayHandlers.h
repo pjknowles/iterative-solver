507f522e8719a4f38a04a14065d06b6e60c1cd55
iakovpolyak <polyaki@cardiff.ac.uk>
2020-08-19 22:24:08 +0100

    Light clean-up

--- src/molpro/linalg/iterativesolver/ArrayHandlers.h
+++ src/molpro/linalg/iterativesolver/ArrayHandlers.h
@@ -37,36 +37,13 @@ struct ArrayHandlersError : public std::logic_error {
 
 } // iterativesolver::util
 
-// TODO We need a constructor that can take any number of handlers explicitly and generate the rest using the factory
-/*!
- * @brief Collection of array handlers needed by IterativeSolver
- * @tparam R array for R space
- * @tparam Q array for Q space
- * @tparam P array for P space
- */
-//template <typename R, typename Q = R, typename P = std::map<size_t, typename R::value_type>>
-//struct ArrayHandlers {
-//  auto& rr() { return *m_rr; }
-//  auto& qq() { return *m_qq; }
-//  auto& pp() { return *m_pp; }
-//  auto& rq() { return *m_rq; }
-//  auto& qr() { return *m_qr; } // TODO qr is not needed, by copy is implemented the wrong way around
-//  auto& rp() { return *m_rp; }
-//  auto& qp() { return *m_qp; }
-//
-//  std::shared_ptr<array::ArrayHandler<R, R>> m_rr;
-//  std::shared_ptr<array::ArrayHandler<Q, Q>> m_qq;
-//  std::shared_ptr<array::ArrayHandler<P, P>> m_pp;
-//  std::shared_ptr<array::ArrayHandler<R, Q>> m_rq;
-//  std::shared_ptr<array::ArrayHandler<R, P>> m_rp;
-//  std::shared_ptr<array::ArrayHandler<Q, R>> m_qr;
-//  std::shared_ptr<array::ArrayHandler<Q, P>> m_qp;
-//};
-
 /*!
  * @brief Class, containing a collection of array handlers used in IterativeSolver
  * Provides a Builder sub-class, containing member functions to accumulate user-defined handlers,
  * as well as determine and instantiate default handlers, based on container (template parameter) types
+ * @tparam R array for R space
+ * @tparam Q array for Q space
+ * @tparam P array for P space
  */
 template <typename R, typename Q = R, typename P = std::map<size_t, typename R::value_type>>
 class ArrayHandlers {

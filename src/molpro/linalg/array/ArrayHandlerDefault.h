545d5f0835461dd236b1fd74ff2e45648f4c46ee
Marat Sibaev <sibaev.marat@gmail.com>
2020-08-20 21:10:01 +0100

    implemented default handler that calls array functions

---
+++ src/molpro/linalg/array/ArrayHandlerDefault.h
@@ -0,0 +1,41 @@
+#ifndef LINEARALGEBRA_SRC_MOLPRO_LINALG_ARRAY_ARRAYHANDLERDEFAULT_H
+#define LINEARALGEBRA_SRC_MOLPRO_LINALG_ARRAY_ARRAYHANDLERDEFAULT_H
+#include <molpro/linalg/array/ArrayHandler.h>
+
+namespace molpro {
+namespace linalg {
+namespace array {
+
+/*!
+ * @brief Fall back handler that calls member functions of arrays
+ */
+template <class AL, class AR = AL>
+class ArrayHandlerDefault : public ArrayHandler<AL, AR> {
+public:
+  using typename ArrayHandler<AL, AR>::value_type_L;
+  using typename ArrayHandler<AL, AR>::value_type_R;
+  using typename ArrayHandler<AL, AR>::value_type;
+  using typename ArrayHandler<AL, AR>::ProxyHandle;
+
+  AL copy(const AR &source) override { return AL{source}; };
+
+  void scal(value_type alpha, AL &x) override { x.scal(alpha); }
+
+  void fill(value_type alpha, AL &x) override { x.fill(alpha); }
+
+  void axpy(value_type alpha, const AR &x, AL &y) override { y.axpy(alpha, x); }
+
+  value_type dot(const AL &x, const AR &y) override { return x.dot(y); }
+
+  ProxyHandle lazy_handle() override { return this->lazy_handle(*this); };
+
+protected:
+  using ArrayHandler<AL, AR>::lazy_handle;
+  using ArrayHandler<AL, AR>::error;
+};
+
+} // namespace array
+} // namespace linalg
+} // namespace molpro
+
+#endif // LINEARALGEBRA_SRC_MOLPRO_LINALG_ARRAY_ARRAYHANDLERDEFAULT_H

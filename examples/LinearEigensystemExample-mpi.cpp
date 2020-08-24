d24adc14c7a3ace10a2a436ff8ae548c99cb9ad3
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2020-08-24 12:06:28 +0100

    clang only compiled, not run, in CI

--- examples/LinearEigensystemExample-mpi.cpp
+++ examples/LinearEigensystemExample-mpi.cpp
@@ -9,7 +9,6 @@
 #include <vector>
 
 // Find lowest eigensolutions of a matrix obtained from an external file
-typedef syntax syntax;
 using Rvector = molpro::linalg::array::DistrArrayMPI3;
 // using Qvector = molpro::linalg::array::DistrArrayHDF5;
 using Qvector = molpro::linalg::array::DistrArrayMPI3;
@@ -153,7 +152,6 @@ int main(int argc, char* argv[]) {
       }
       if (mpi_rank == 0)
         std::cout << solver.statistics() << std::endl;
-      deliberate syntax error;
     }
   }
   MPI_Finalize();

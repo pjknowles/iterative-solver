42edfc00323f211ee3bb867bbb820707be617306
Marat Sibaev <sibaev.marat@gmail.com>
2020-09-15 17:33:17 +0100

    fix constness of util::wrap

--- src/molpro/linalg/itsolv/subspace/util.h
+++ src/molpro/linalg/itsolv/subspace/util.h
@@ -10,20 +10,33 @@ namespace util {
 
 //! Takes a vector of containers and returns a vector of references to each element
 template <class R>
-std::vector<std::reference_wrapper<R>> wrap(const std::vector<R>& vec) {
-  auto w = std::vector<std::reference_wrapper<R>>{};
-  for (const auto& v : vec) {
-    w.emplace_back(v);
-  }
+auto wrap(const std::vector<R>& vec) {
+  auto w = std::vector<std::reference_wrapper<const R>>{};
+  std::copy(begin(vec), end(vec), std::back_inserter(w));
   return w;
 }
+
+//! Takes a vector of containers and returns a vector of references to each element
+template <class R>
+auto wrap(std::vector<R>& vec) {
+  auto w = std::vector<std::reference_wrapper<R>>{};
+  std::copy(begin(vec), end(vec), std::back_inserter(w));
+  return w;
+}
+
 //! Takes a map of containers and returns a vector of references to each element in the same order
 template <class R>
-std::vector<std::reference_wrapper<R>> wrap(const std::map<size_t, R>& vec) {
+auto wrap(const std::map<size_t, R>& vec) {
+  auto w = std::vector<std::reference_wrapper<const R>>{};
+  std::transform(begin(vec), end(vec), std::back_inserter(w), [](const auto& v) { return v.second; });
+  return w;
+}
+
+//! Takes a map of containers and returns a vector of references to each element in the same order
+template <class R>
+auto wrap(std::map<size_t, R>& vec) {
   auto w = std::vector<std::reference_wrapper<R>>{};
-  for (const auto& v : vec) {
-    w.emplace_back(v.second);
-  }
+  std::transform(begin(vec), end(vec), std::back_inserter(w), [](const auto& v) { return v.second; });
   return w;
 }
 
@@ -31,16 +44,14 @@ std::vector<std::reference_wrapper<R>> wrap(const std::map<size_t, R>& vec) {
 template <class R>
 std::vector<std::reference_wrapper<R>> wrap(const std::map<size_t, std::reference_wrapper<R>>& vec) {
   auto w = std::vector<std::reference_wrapper<R>>{};
-  for (const auto& v : vec) {
-    w.emplace_back(v.second);
-  }
+  std::transform(begin(vec), end(vec), std::back_inserter(w), [](const auto& v) { return v.second; });
   return w;
 }
 
 //! Calculates overlap matrix between left and right vectors
 template <class R, class Q>
 Matrix<double> overlap(const std::vector<std::reference_wrapper<R>>& left,
-                       const std::vector<std::reference_wrapper<Q>>& right, const array::ArrayHandler<R, Q>& handler) {
+                       const std::vector<std::reference_wrapper<Q>>& right, array::ArrayHandler<R, Q>& handler) {
   auto m = Matrix<double>({left.size(), right.size()});
   for (size_t i = 0; i < m.rows(); ++i)
     for (size_t j = 0; j < m.cols(); ++j)
@@ -50,7 +61,7 @@ Matrix<double> overlap(const std::vector<std::reference_wrapper<R>>& left,
 
 //! Calculates overlap matrix for a parameter set. Matrix is symmetric by construction.
 template <class R>
-Matrix<double> overlap(const std::vector<std::reference_wrapper<R>>& params, const array::ArrayHandler<R, R>& handler) {
+Matrix<double> overlap(const std::vector<std::reference_wrapper<R>>& params, array::ArrayHandler<R, R>& handler) {
   auto m = Matrix<double>({params.size(), params.size()});
   for (size_t i = 0; i < m.rows(); ++i)
     for (size_t j = 0; j <= i; ++j)

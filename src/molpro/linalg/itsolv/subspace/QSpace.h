553e9851bb5688972e4186142831f48341428b23
Marat Sibaev <sibaev.marat@gmail.com>
2020-09-16 22:21:49 +0100

    calculates subspace data in update()

--- src/molpro/linalg/itsolv/subspace/QSpace.h
+++ src/molpro/linalg/itsolv/subspace/QSpace.h
@@ -51,16 +51,17 @@ struct QParam {
 template <class R, class Q, class P>
 struct QSpace {
   using typename RSpace<R, Q, P>::VecRefR;
+  using VecRefQ = std::vector<std::reference_wrapper<Q>>;
+
   SubspaceData data = null_data<EqnData::H, EqnData::S>();
   SubspaceData qr = null_data<EqnData::H, EqnData::S>(); //!< QxR section of subspace data
   SubspaceData rq = null_data<EqnData::H, EqnData::S>(); //!< RxQ section of subspace data
 
   void update(const RSpace<R, Q, P>& rs, IterativeSolver<R, Q, P>& solver) {
-    qr = null_data<EqnData::H, EqnData::S>();
-    rq = null_data<EqnData::H, EqnData::S>();
     auto& dummy = rs.dummy(2);
     auto& qparam = dummy[0];
     auto& qaction = dummy[1];
+    m_used_working_set.clear();
     for (size_t i = 0; i < rs.working_set().size(); ++i) {
       auto& param = rs.params()[i];
       auto& action = rs.actions()[i];
@@ -83,9 +84,56 @@ struct QSpace {
                                      norm,
                                      1};
         m_params.emplace_back(std::move(q));
+        m_used_working_set.emplace_back(rs.working_set()[i]);
       }
     }
-    // update subspace data. This has to be done with all new parameters for efficiency.
+    auto old_params = VecRefQ{};
+    auto old_actions = VecRefQ{};
+    auto iq = m_params.begin();
+    auto nR = rs.size();
+    auto nQ = m_params.size();
+    auto nQprev = nQ - m_used_working_set.size();
+    auto end = std::next(m_params.begin(), nQprev);
+    for (; iq != end; ++iq) {
+      old_params.emplace(*iq->param);
+      old_actions.emplace(*iq->action);
+    }
+    auto new_params = VecRefQ{};
+    auto new_actions = VecRefQ{};
+    for (; iq != m_params.end(); ++iq) {
+      new_params.emplace(*iq->param);
+      new_actions.emplace(*iq->action);
+    }
+    auto all_params = VecRefQ{};
+    auto all_actions = VecRefQ{};
+    for (auto it = m_params.begin(); it != m_params.end(); ++it) {
+      all_params.emplace_back(it->param);
+      all_actions.emplace_back(it->actions);
+    }
+    auto ov_params_old_new = util::overlap(old_params, new_params, m_handlers->qq());
+    auto ov_actions_old_new = util::overlap(old_actions, new_actions, m_handlers->qq());
+    auto ov_params_new_old = util::overlap(new_params, old_params, m_handlers->qq());
+    auto ov_actions_new_old = util::overlap(new_actions, old_actions, m_handlers->qq());
+    auto ov_params_new_new = util::overlap(new_params, new_params, m_handlers->qq());
+    auto ov_actions_new_new = util::overlap(new_actions, new_actions, m_handlers->qq());
+    auto ov_params_qr = util::overlap(all_params, rs.params, m_handlers()->qr());
+    auto ov_actions_qr = util::overlap(all_actions, rs.actions, m_handlers()->qr());
+    // FIXME in hermitian cases rq is redundant
+    auto ov_params_rq = util::overlap(rs.params, all_params, m_handlers()->rq());
+    auto ov_actions_rq = util::overlap(rs.actions, all_actions, m_handlers()->rq());
+    data[EqnData::S].resize({nQ, nQ});
+    data[EqnData::H].resize({nQ, nQ});
+    data[EqnData::S].slice({nQprev, 0}, {nQ, nQprev}) = ov_params_new_old;
+    data[EqnData::H].slice({nQprev, 0}, {nQ, nQprev}) = ov_actions_new_old;
+    data[EqnData::S].slice({0, nQprev}, {nQprev, nQ}) = ov_params_old_new;
+    data[EqnData::H].slice({0, nQprev}, {nQprev, nQ}) = ov_actions_old_new;
+    data[EqnData::S].slice({nQprev, nQprev}, {nQ, nQ}) = ov_params_new_new;
+    qr[EqnData::S].resize({nQ, nR});
+    qr[EqnData::H].resize({nQ, nR});
+    qr[EqnData::S].slice() = ov_params_qr;
+    qr[EqnData::H].slice() = ov_actions_qr;
+    rq[EqnData::S].slice() = ov_params_rq;
+    rq[EqnData::H].slice() = ov_actions_rq;
   }
 
   void add_converged(const R& param, const R& action, size_t root) {
@@ -99,10 +147,15 @@ struct QSpace {
     // update qq
   }
 
+  //! Vector of root indices for r vectors that were used to generate new q vectors. Converged solutions are not
+  //! included.
+  auto& used_working_set() const { return m_used_working_set; }
+
 protected:
   std::shared_ptr<ArrayHandlers<R, Q, P>> m_handlers;
-  std::list<qspace::QParam<Q>> m_params; //!< q vectors constructed as differences
-  bool m_orthogonalise = true;           //!< whether to orthogonalise a new Q vector relative to its R vector
+  std::vector<size_t> m_used_working_set; //!< root indices of r vectors that were used to generate new q vectors
+  std::list<qspace::QParam<Q>> m_params;  //!< q vectors constructed as differences
+  bool m_orthogonalise = true;            //!< whether to orthogonalise a new Q vector relative to its R vector
 };
 
 } // namespace subspace

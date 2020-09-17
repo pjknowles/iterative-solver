12a4ab132538632dc021145c90429aa14ce88ca6
Marat Sibaev <sibaev.marat@gmail.com>
2020-09-17 12:02:21 +0100

    move DummySolver out of testRSpace.cpp to its own header

--- test/itsolv/subspace/testRSpace.cpp
+++ test/itsolv/subspace/testRSpace.cpp
@@ -1,11 +1,10 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "DummySolver.h"
 #include <molpro/linalg/itsolv/subspace/RSpace.h>
 
 using molpro::linalg::itsolv::ArrayHandlers;
-using molpro::linalg::itsolv::IterativeSolver;
-using molpro::linalg::itsolv::Statistics;
 using molpro::linalg::itsolv::subspace::EqnData;
 using molpro::linalg::itsolv::subspace::Matrix;
 using molpro::linalg::itsolv::subspace::RSpace;
@@ -15,40 +14,6 @@ using ::testing::Each;
 using ::testing::Eq;
 using ::testing::Ne;
 using ::testing::Pointwise;
-
-/*
- * Things to test:
- *  - update adds new parameters
- *  - update adds new parameters
- */
-
-template <class R, class Q, class P>
-struct DummySolver : IterativeSolver<R, Q, P> {
-  using typename IterativeSolver<R, Q, P>::value_type;
-  using typename IterativeSolver<R, Q, P>::scalar_type;
-  size_t add_vector(std::vector<R>& parameters, std::vector<R>& action) override { return 0; };
-  size_t add_vector(std::vector<R>& parameters, std::vector<R>& action, std::vector<P>& parametersP) override {
-    return 0;
-  };
-  size_t add_p(std::vector<P>& Pvectors, const value_type* PP, std::vector<R>& parameters, std::vector<R>& action,
-               std::vector<P>& parametersP) override {
-    return 0;
-  };
-  void solution(const std::vector<int>& roots, std::vector<R>& parameters, std::vector<R>& residual) override{};
-  void solution(const std::vector<int>& roots, std::vector<R>& parameters, std::vector<R>& residual,
-                std::vector<P>& parametersP) override{};
-  std::vector<size_t> suggest_p(const std::vector<R>& solution, const std::vector<R>& residual, size_t maximumNumber,
-                                double threshold) override {
-    return {};
-  };
-
-  const std::vector<int>& working_set() const override { return ws; };
-  const std::vector<scalar_type>& errors() const override { return er; };
-  const Statistics& statistics() const override { return st; };
-  std::vector<int> ws;
-  std::vector<scalar_type> er;
-  Statistics st;
-};
 
 struct RSpaceF : ::testing::Test {
   using R = std::vector<double>;

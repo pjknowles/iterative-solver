9dca8a2b3f9426d44f6627b04c9ba52d2e1e18d9
Marat Sibaev <sibaev.marat@gmail.com>
2020-10-07 15:43:22 +0100

    outline skeleton of XSpace; expose subspace containers publicly

--- src/molpro/linalg/itsolv/subspace/XSpace.h
+++ src/molpro/linalg/itsolv/subspace/XSpace.h
@@ -3,6 +3,8 @@
 #include <cassert>
 #include <molpro/linalg/itsolv/helper.h>
 #include <molpro/linalg/itsolv/subspace/CSpace.h>
+#include <molpro/linalg/itsolv/subspace/PSpace.h>
+#include <molpro/linalg/itsolv/subspace/QSpace.h>
 #include <molpro/linalg/itsolv/subspace/XSpaceI.h>
 #include <molpro/linalg/itsolv/subspace/build_subspace.h>
 #include <molpro/linalg/itsolv/subspace/check_conditioning.h>
@@ -18,14 +20,21 @@ public:
   using typename XSpaceI<R, Q, P>::value_type;
   using typename XSpaceI<R, Q, P>::value_type_abs;
 
-  explicit XSpace(std::shared_ptr<Logger> logger) : m_logger(std::move(logger)) {
+  explicit XSpace(const std::shared_ptr<ArrayHandlers<R, Q, P>>& handlers, const std::shared_ptr<Logger>& logger)
+      : pspace(), qspace(handlers, logger), cspace(handlers, logger), m_logger(logger) {
     this->data = null_data<EqnData::H, EqnData::S>();
   };
 
-  void update_qspace(const std::vector<R>& params, const std::vector<R>& actions) override {}
+  void update_qspace(const std::vector<R>& params, const std::vector<R>& actions) override {
+    qspace.update(wrap(params), wrap(actions));
+    // update data
+  }
 
   void update_cspace(const std::vector<unsigned int>& roots, const std::vector<R>& params,
-                     const std::vector<R>& actions, const std::vector<value_type>& errors) override {}
+                     const std::vector<R>& actions, const std::vector<value_type>& errors) override {
+    cspace.update(roots, params, actions, m_eval, this->data, m_evec, m_dim, errors);
+    // update data
+  }
 
   void check_conditioning() override {
     auto nX_on_entry = m_dim.nX;
@@ -45,13 +54,14 @@ public:
   }
 
   //! Solves the underlying problem in the subspace. solver can be used to pass extra parameters defining the problem
-  void solve(const IterativeSolver<R, Q, P>& solver) override {
+  void solve(const IterativeSolver<R, Q, P>& solver) {
     assert("XSpaceLinEig can only be used with LinearEigensystem solver");
   };
 
   //! Solves the linear eigensystem problem in the subspace.
   void solve(const LinearEigensystem<R, Q, P>& solver) {
     m_logger->msg("XSpaceLinEig::solve", Logger::Trace);
+    check_conditioning();
     auto& h = this->data[EqnData::H];
     auto& s = this->data[EqnData::S];
     if (m_hermitian)
@@ -88,45 +98,46 @@ public:
   }
 
   void eraseq(size_t i) override {
-    m_qspace.erase(i);
+    qspace.erase(i);
     // remove data
   }
 
   void erasep(size_t i) override {
-    m_pspace.erase(i);
+    pspace.erase(i);
     // remove data
   }
 
   void erasec(size_t i) override {
-    m_cspace.erase(i);
+    cspace.erase(i);
     // remove data
   }
 
-  VecRef<P> paramsp() override { return m_pspace.params(); }
-  VecRef<P> actionsp() override { return m_pspace.actions(); }
-  VecRef<Q> paramsq() override { return m_qspace.params(); }
-  VecRef<Q> actionsq() override { return m_qspace.actions(); }
-  VecRef<Q> paramsc() override { return m_cspace.params(); }
-  VecRef<Q> actionsc() override { return m_cspace.actions(); }
+  VecRef<P> paramsp() override { return pspace.params(); }
+  VecRef<P> actionsp() override { return pspace.actions(); }
+  VecRef<Q> paramsq() override { return qspace.params(); }
+  VecRef<Q> actionsq() override { return qspace.actions(); }
+  VecRef<Q> paramsc() override { return cspace.params(); }
+  VecRef<Q> actionsc() override { return cspace.actions(); }
 
-  CVecRef<P> paramsp() const override { return m_pspace.params(); }
-  CVecRef<P> actionsp() const override { return m_pspace.actions(); }
-  CVecRef<Q> paramsq() const override { return m_qspace.params(); }
-  CVecRef<Q> actionsq() const override { return m_qspace.actions(); }
-  CVecRef<Q> paramsc() const override { return m_cspace.cparams(); }
-  CVecRef<Q> actionsc() const override { return m_cspace.cactions(); }
+  CVecRef<P> paramsp() const override { return pspace.params(); }
+  CVecRef<P> actionsp() const override { return pspace.actions(); }
+  CVecRef<Q> paramsq() const override { return qspace.params(); }
+  CVecRef<Q> actionsq() const override { return qspace.actions(); }
+  CVecRef<Q> paramsc() const override { return cspace.cparams(); }
+  CVecRef<Q> actionsc() const override { return cspace.cactions(); }
 
-  CVecRef<P> cparamsp() const override { return m_pspace.cparams(); }
-  CVecRef<P> cactionsp() const override { return m_pspace.cactions(); }
-  CVecRef<Q> cparamsq() const override { return m_qspace.cparams(); }
-  CVecRef<Q> cactionsq() const override { return m_qspace.cactions(); }
-  CVecRef<Q> cparamsc() const override { return m_cspace.cparams(); }
-  CVecRef<Q> cactionsc() const override { return m_cspace.cactions(); }
+  CVecRef<P> cparamsp() const override { return pspace.cparams(); }
+  CVecRef<P> cactionsp() const override { return pspace.cactions(); }
+  CVecRef<Q> cparamsq() const override { return qspace.cparams(); }
+  CVecRef<Q> cactionsq() const override { return qspace.cactions(); }
+  CVecRef<Q> cparamsc() const override { return cspace.cparams(); }
+  CVecRef<Q> cactionsc() const override { return cspace.cactions(); }
+
+  PSpace<R, P> pspace;
+  QSpace<R, Q, P> qspace;
+  CSpace<R, Q, P, value_type> cspace;
 
 protected:
-  CSpace<R, Q, P, value_type> m_cspace;
-  QSpace<R, Q, P> m_qspace;
-  PSpace<R, P> m_pspace;
   std::shared_ptr<Logger> m_logger;
   xspace::Dimensions m_dim;
   bool m_hermitian = false; //!< whether the matrix is Hermitian

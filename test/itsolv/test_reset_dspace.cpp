a3de152c70ee8438e466a79447c72dd4c37b3f8b
Marat Sibaev <sibaev.marat@gmail.com>
2020-10-29 12:48:42 +1300

    unit test construct_projected_solutions

--- test/itsolv/test_reset_dspace.cpp
+++ test/itsolv/test_reset_dspace.cpp
@@ -3,6 +3,7 @@
 
 #include <molpro/linalg/itsolv/reset_dspace.h>
 
+using molpro::linalg::itsolv::detail::construct_projected_solutions;
 using molpro::linalg::itsolv::detail::DoReset;
 using molpro::linalg::itsolv::subspace::Matrix;
 using molpro::linalg::itsolv::subspace::xspace::Dimensions;
@@ -44,4 +45,27 @@ TEST(itsolv_reset_dspace, DoReset_update) {
   d.update(3, max_q, 0);
   ASSERT_FALSE(d.value());
   ASSERT_EQ(max_q, init_max_q);
+}
+
+TEST(itsolv_reset_dspace, construct_projected_solutions) {
+  const size_t nC = 2;
+  const auto dims = Dimensions(1, 1, 2);
+  const auto solutions = Matrix<double>({1.0, 1.0, 1.0, 1.0, //
+                                         0.1, 0.2, 1.0, -1.0},
+                                        {nC, dims.nX});
+  const auto overlap = Matrix<double>({1, 0, 1, 0, //
+                                       0, 1, 1, 0, //
+                                       1, 1, 4, 1, //
+                                       0, 0, 1, 1},
+                                      {dims.nX, dims.nX});
+  const auto ref_overlap_CC = Matrix<double>({7., 3., //
+                                              3., 3.},
+                                             {nC, nC});
+  const auto ref_proj_solutions = Matrix<double>({1.0, 1.0, //
+                                                  1.0, -1.0},
+                                                 {nC, dims.nD});
+  Matrix<double> res_proj_solutions, res_overlap_CC;
+  std::tie(res_proj_solutions, res_overlap_CC) = construct_projected_solutions(solutions, overlap, dims);
+  ASSERT_THAT(res_proj_solutions.data(), Pointwise(DoubleEq(), ref_proj_solutions.data()));
+  ASSERT_THAT(res_overlap_CC.data(), Pointwise(DoubleEq(), ref_overlap_CC.data()));
 }

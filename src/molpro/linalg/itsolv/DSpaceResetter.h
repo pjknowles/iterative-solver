ed9d5eb84e357896338ee6c62296cdce66e3c052
Marat Sibaev <sibaev.marat@gmail.com>
2020-10-30 15:07:42 +1300

    bugs

--- src/molpro/linalg/itsolv/DSpaceResetter.h
+++ src/molpro/linalg/itsolv/DSpaceResetter.h
@@ -46,16 +46,17 @@ public:
                Logger::Trace);
     const auto nC = solutions.rows();
     auto dims = xspace.dimensions();
-    if (solution_params.empty()) {
+    if (solution_params.empty() && !rparams.empty()) {
       logger.msg("constructing solutions", Logger::Debug);
       for (size_t i = 0; i < nC; ++i)
-        solution_params.emplace_back(construct_zeroed_Q(rparams.first()), handlers.qr());
+        solution_params.emplace_back(util::construct_zeroed_copy(rparams.front(), handlers.qr()));
       auto roots = std::vector<unsigned int>(nC);
       std::iota(begin(roots), end(roots), 0);
-      util::construct_solutions(wrap(begin(solution_params), end(solution_params)), roots, solutions, xspace.cparamsp(),
-                                xspace.cparamsq(), xspace.cparamsd(), dims.oP, dims.oQ, dims.oD, handlers.qq(),
-                                handlers.qp(), handlers.qq());
-      xspace.update_dspace({}, {}, {});
+      util::construct_solutions(wrap<Q>(begin(solution_params), end(solution_params)), roots, solutions,
+                                xspace.cparamsp(), xspace.cparamsq(), xspace.cparamsd(), dims.oP, dims.oQ, dims.oD,
+                                handlers.qq(), handlers.qp(), handlers.qq());
+      VecRef<R> null_params, null_actions;
+      xspace.update_dspace(null_params, null_actions, {});
     }
     const auto nR = std::min(rparams.size(), solution_params.size());
     for (size_t i = 0; i < nR; ++i) {
@@ -63,7 +64,7 @@ public:
       solution_params.pop_front();
     }
     const auto wparams = cwrap<R>(begin(rparams), begin(rparams) + nR);
-    const auto overlap = subspace::util::overlap(wparams, xspace.cparamsq(), handlers.rq());
+    auto overlap = subspace::util::overlap(wparams, xspace.cparamsq(), handlers.rq());
     for (size_t i = 0; i < nR && xspace.dimensions().nQ > 0; ++i) {
       auto* row_start = &overlap(i, xspace.dimensions().oQ);
       auto* row_end = row_start + xspace.dimensions().nQ;
@@ -71,7 +72,8 @@ public:
       auto i_max = std::distance(row_start, it_max);
       xspace.eraseq(i_max);
       overlap.remove_col(xspace.dimensions().oQ + i_max);
-      logger.msg("removed q index = " + std::to_string(i_max) + ", with overlap = " + std::to_string(*it_max));
+      logger.msg("removed q index = " + std::to_string(i_max) + ", with overlap = " + std::to_string(*it_max),
+                 Logger::Debug);
     }
     // FIXME It might be prudent to double check that the subspace is stable and remove more Q vectors if it is not
     auto new_working_set = std::vector<unsigned int>(nR);

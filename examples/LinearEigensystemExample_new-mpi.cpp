1edfe464e42ea033984efbf0b6688455df2b2b07
iakovpolyak <polyaki@cardiff.ac.uk>
2020-11-06 12:27:17 +0000

    Parallel tests now work (both for Qspace being an MPI3 and HDF5 container)

--- examples/LinearEigensystemExample_new-mpi.cpp
+++ examples/LinearEigensystemExample_new-mpi.cpp
@@ -11,8 +11,7 @@
 
 // Find lowest eigensolutions of a matrix obtained from an external file
 using Rvector = molpro::linalg::array::DistrArrayMPI3;
-//using Qvector = molpro::linalg::array::DistrArrayHDF5;
-using Qvector = molpro::linalg::array::DistrArrayMPI3;
+using Qvector = molpro::linalg::array::DistrArrayHDF5;
 using Pvector = std::map<size_t, double>;
 int n; // dimension of problem
 constexpr bool collective_algorithm = false;

bdaedd5c5924665e760dab8b85820aea9bf72910
Marat Sibaev <sibaev.marat@gmail.com>
2020-11-10 21:56:01 +1300

    implemented PSpace class

--- src/molpro/linalg/itsolv/subspace/PSpace.h
+++ src/molpro/linalg/itsolv/subspace/PSpace.h
@@ -1,25 +1,35 @@
 #ifndef LINEARALGEBRA_SRC_MOLPRO_LINALG_ITSOLV_SUBSPACE_PSPACE_H
 #define LINEARALGEBRA_SRC_MOLPRO_LINALG_ITSOLV_SUBSPACE_PSPACE_H
+#include <molpro/linalg/array/ArrayHandler.h>
 #include <molpro/linalg/itsolv/subspace/SubspaceData.h>
 #include <molpro/linalg/itsolv/wrap.h>
 
 namespace molpro::linalg::itsolv::subspace {
 
 template <class Rt, class Pt>
-struct PSpace {
+class PSpace {
+public:
   using R = Rt;
   using P = Pt;
 
-  CVecRef<P> params() const { return {}; }
+  void update(const CVecRef<P>& params, array::ArrayHandler<P, P>& handler) {
+    for (const auto& p : params)
+      m_params.emplace_back(handler.copy(p));
+  }
+
+  CVecRef<P> params() const { return cwrap(m_params); }
   CVecRef<P> cparams() const { return params(); }
-  VecRef<P> params() { return {}; }
+  VecRef<P> params() { return wrap(m_params); }
   CVecRef<P> actions() const { return {}; }
   CVecRef<P> cactions() const { return actions(); }
   VecRef<P> actions() { return {}; }
 
-  size_t size() const { return 0; }
+  size_t size() const { return m_params.size(); }
 
-  void erase(size_t i) {}
+  void erase(size_t i) { m_params.erase(m_params.begin() + i); }
+
+private:
+  std::vector<P> m_params;
 };
 
 } // namespace molpro::linalg::itsolv::subspace

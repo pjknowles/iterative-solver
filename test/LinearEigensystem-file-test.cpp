2a06afd00dd4257a16c1538cf5d8f46b8c7beb76
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2020-11-17 13:53:14 +0000

    another try

--- test/LinearEigensystem-file-test.cpp
+++ test/LinearEigensystem-file-test.cpp
@@ -174,7 +174,9 @@ void test_eigen(const std::string& title = "") {
           using molpro::linalg::itsolv::CVecRef;
           using molpro::linalg::itsolv::VecRef;
 //          using fapply_on_p_type = std::function<void(const std::vector<VectorP>&, const CVecRef<P>&, const VecRef<R>&)>;
-          auto apply_p = [](const std::vector<vectorP>& pvectors, const CVecRef<Pvector>& pspace, VecRef<Rvector>& action) {
+          std::function<void(const std::vector<vectorP>&, const CVecRef<Pvector>&, const VecRef<Rvector>&)>
+//          auto
+          apply_p = [](const std::vector<vectorP>& pvectors, const CVecRef<Pvector>& pspace, VecRef<Rvector>& action) {
             for (size_t i = 0; i < pvectors.size(); i++) {
               auto& actioni = (action[i]).get();
               for (size_t pi = 0; pi < pspace.size(); pi++) {

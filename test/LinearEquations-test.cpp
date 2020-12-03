7b95712cc6c09983a5d828c7eb3ec78a2e99fde5
Marat Sibaev <sibaev.marat@gmail.com>
2020-12-03 14:21:50 +1300

    finish skeletion of the test without using iterative solver

--- test/LinearEquations-test.cpp
+++ test/LinearEquations-test.cpp
@@ -3,6 +3,7 @@
 
 #include <Eigen/Dense>
 #include <Eigen/Eigenvalues>
+#include <numeric>
 
 #include <molpro/linalg/itsolv/helper.h>
 
@@ -48,7 +49,7 @@ auto solve_full_problem(const MatrixXdr& mat, const MatrixXdr& rhs, bool augment
   matrix.resize(nX * nX);
   metric.resize(nX * nX);
   rhs_flat.resize(nX * nroot);
-  for (size_t i = 0, ij = 0; i < nX; ++i)
+  for (size_t i = 0; i < nX; ++i)
     metric[i * (nX + 1)] = 1;
   for (size_t i = 0, ij = 0; i < nX; ++i)
     for (size_t j = 0; j < nX; ++j, ++ij)
@@ -68,12 +69,56 @@ auto solve_full_problem(const MatrixXdr& mat, const MatrixXdr& rhs, bool augment
   return solutions_mat;
 }
 
+std::vector<double> residual(const MatrixXdr& rhs, const std::vector<Rvector>& actions) {
+  const auto nroots = actions.size();
+  const auto nX = rhs.rows();
+  auto errors = std::vector<double>(nroots);
+  for (size_t i = 0; i < nroots; ++i) {
+    double norm = 0;
+    for (size_t j = 0; j < nX; ++j) {
+      errors[i] += rhs(i, j) - actions[i][j];
+      norm += rhs(i, j) * rhs(i, j);
+    }
+    if (norm != 0)
+      errors[i] /= norm;
+    errors[i] = std::sqrt(std::abs(errors[i]));
+  }
+  return errors;
+}
+
 void run_test(const MatrixXdr& mat, const MatrixXdr& rhs, const Update& update, bool augmented_hessian) {
   const auto n_root_max = rhs.cols();
+  const auto nX = mat.rows();
   auto reference_solutions = solve_full_problem(mat, rhs, augmented_hessian);
   for (size_t nroot = 1; nroot <= n_root_max; ++nroot) {
     // solve the problem iteratively
     // compare solutions
+    std::vector<std::vector<double>> parameters, actions;
+    std::vector<int> roots;
+    //    for (int root = 0; root < solver->n_roots(); root++) {
+    for (int root = 0; root < nroot; root++) {
+      parameters.emplace_back(nX);
+      actions.emplace_back(nX);
+      for (size_t i = 0; i < nX; ++i) {
+        parameters[root][i] = reference_solutions[root][i];
+        actions[root][i] = rhs(i, root);
+      }
+      roots.push_back(root);
+    }
+    //    solver->solution(roots, parameters, actions);
+    auto residual_errors = residual(rhs, actions);
+    EXPECT_EQ(residual_errors.size(), nroot);
+    EXPECT_THAT(residual_errors, ::testing::Each(::testing::Le(1.))); // options->convergence_threshold);
+    for (size_t i = 0; i < nroot; ++i) {
+      auto norm = std::inner_product(reference_solutions.at(i).begin(), reference_solutions.at(i).end(),
+                                     reference_solutions.at(i).begin(), 0.);
+      if (norm != 0.) {
+        auto overlap_with_reference =
+            std::inner_product(parameters.at(i).begin(), parameters.at(i).end(), reference_solutions.at(i).begin(), 0.);
+        overlap_with_reference = std::sqrt(std::abs(overlap_with_reference / norm));
+        EXPECT_NEAR(overlap_with_reference, 1., 1) << "root = " << i; // options->convergence_threshold.value());
+      }
+    }
   }
 }
 } // namespace
@@ -81,12 +126,10 @@ void run_test(const MatrixXdr& mat, const MatrixXdr& rhs, const Update& update, 
 TEST(LinearEquations, simple_symmetric_system) {
   double p = 1;
   size_t n_max = 4;
-  for (size_t n = 0; n < n_max; ++n) {
+  bool augmented_hessian = false;
+  for (size_t n = 1; n < n_max; ++n) {
     auto [mat, rhs] = construct_simple_symmetric_system(n, p);
     auto update = [](const auto& params, const auto& actions, auto n_work) {};
-    // choose matrix apply function
-    // choose update functon
-    // run test
-    run_test(mat, rhs, update, false);
+    run_test(mat, rhs, update, augmented_hessian);
   }
 }

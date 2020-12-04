dcadbf85feba005dd26c872b365cf9a8d5e3f49a
Marat Sibaev <sibaev.marat@gmail.com>
2020-12-04 15:18:18 +1300

    add pathway to solve linear equations to SubspaceSolverLinEig

--- src/molpro/linalg/itsolv/subspace/SubspaceSolverLinEig.h
+++ src/molpro/linalg/itsolv/subspace/SubspaceSolverLinEig.h
@@ -6,6 +6,9 @@
 
 namespace molpro::linalg::itsolv::subspace {
 
+/*!
+ * @brief Solves subspace problem for linear eigenvalues and system of linear equations
+ */
 template <class RT, class QT, class PT>
 class SubspaceSolverLinEig : public ISubspaceSolver<RT, QT, PT> {
 public:
@@ -19,11 +22,22 @@ public:
 
   void solve(IXSpace<R, Q, P>& xspace, const size_t nroots_max) override {
     m_logger->msg("SubspaceSolverLinEig::solve", Logger::Trace);
+    if (xspace.data.find(EqnData::rhs) == xspace.data.end() || xspace.data[EqnData::rhs].empty()) {
+      solve_eigenvalue(xspace, nroots_max);
+    } else {
+      solve_linear_equations(xspace);
+    }
+  }
+
+protected:
+  void solve_eigenvalue(IXSpace<R, Q, P>& xspace, const size_t nroots_max) {
+    m_logger->msg("SubspaceSolverLinEig::solve_eigenvalue", Logger::Trace);
     auto h = xspace.data[EqnData::H];
     auto s = xspace.data[EqnData::S];
     if (m_logger->data_dump) {
       m_logger->msg("S = " + as_string(s), Logger::Info);
       m_logger->msg("H = " + as_string(h, 15), Logger::Info);
+      m_logger->msg("rhs = " + as_string(h, 15), Logger::Info);
     }
     auto dim = h.rows();
     auto evec = std::vector<value_type>{};
@@ -42,6 +56,32 @@ public:
     }
   }
 
+  void solve_linear_equations(IXSpace<R, Q, P>& xspace) {
+    m_logger->msg("SubspaceSolverLinEig::solve_linear_equations", Logger::Trace);
+    auto h = xspace.data[EqnData::H];
+    auto s = xspace.data[EqnData::S];
+    auto rhs = xspace.data[EqnData::rhs];
+    if (m_logger->data_dump) {
+      m_logger->msg("S = " + as_string(s, 15), Logger::Info);
+      m_logger->msg("H = " + as_string(h, 15), Logger::Info);
+      m_logger->msg("rhs = " + as_string(rhs, 15), Logger::Info);
+    }
+    const auto dim = h.rows();
+    const auto n_solutions = rhs.cols();
+    auto solution = std::vector<value_type>{};
+    m_eigenvalues.assign(n_solutions, 0);
+    int verbosity = m_logger->max_trace_level == Logger::Info ? 3 : 0;
+    itsolv::solve_LinearEquations(solution, m_eigenvalues, h.data(), s.data(), rhs.data(), dim, n_solutions,
+                                  m_augmented_hessian, m_svd_solver_threshold, verbosity);
+    m_solutions = Matrix<value_type>{std::move(solution), {n_solutions, dim}};
+    m_errors.assign(size(), std::numeric_limits<value_type_abs>::max());
+    if (m_logger->data_dump) {
+      m_logger->msg("eigenvalues = ", begin(m_eigenvalues), end(m_eigenvalues), Logger::Debug, 10);
+      m_logger->msg("solutions = " + as_string(m_solutions), Logger::Info);
+    }
+  }
+
+public:
   //! Set error value for solution *root*
   void set_error(int root, value_type_abs error) override { m_errors.at(root) = error; }
   void set_error(const std::vector<int>& roots, const std::vector<value_type_abs>& errors) override {
@@ -60,17 +100,22 @@ public:
   //! Set Hermiticity of the subspace.
   void set_hermiticity(bool hermitian) { m_hermitian = hermitian; }
   bool get_hermiticity() { return m_hermitian; }
+  //! Set value of augmented hessian parameter. If 0, than augmented Hessian is not used.
+  void set_augmented_hessian(value_type_abs parameter) { m_augmented_hessian = parameter; }
+  value_type_abs get_augmented_hessian() { return m_augmented_hessian; }
 
 protected:
   Matrix<value_type> m_solutions;        //!< solution matrix with row vectors
   std::vector<value_type> m_eigenvalues; //!< eigenvalues
   std::vector<value_type_abs> m_errors;  //!< errors in subspace solutions
-  std::shared_ptr<Logger> m_logger;
+  std::shared_ptr<Logger> m_logger{};
 
 public:
   value_type_abs m_svd_solver_threshold = 1.0e-14; //!< threshold to select null space during SVD in eigenproblem
 protected:
   bool m_hermitian = true; //!< flags the matrix as Hermitian
+  value_type_abs m_augmented_hessian =
+      0; //!< value of augmented hessian parameter. If 0, than augmented Hessian is not used
 };
 
 } // namespace molpro::linalg::itsolv::subspace

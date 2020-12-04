06ba8807360767a7d4d46fd8cf9b50835d342642
Marat Sibaev <sibaev.marat@gmail.com>
2020-12-04 17:08:11 +1300

    use solver in the test. Test is failing due to normalisation of parameters

--- test/LinearEquations-test.cpp
+++ test/LinearEquations-test.cpp
@@ -55,8 +55,8 @@ auto construct_simple_symmetric_system(size_t n, double p) {
 void apply_matrix(const MatrixXdr& mat, const std::vector<Rvector>& params, std::vector<Rvector>& actions,
                   size_t n_work) {
   for (size_t i = 0; i < n_work; ++i) {
-    auto x = Eigen::Map<const Eigen::VectorXd>(params.at(i).data(), 1, params[i].size());
-    auto r = Eigen::Map<Eigen::VectorXd>(actions.at(i).data(), 1, params[i].size());
+    auto x = Eigen::Map<const Eigen::VectorXd>(params.at(i).data(), params[i].size());
+    auto r = Eigen::Map<Eigen::VectorXd>(actions.at(i).data(), params[i].size());
     r += mat * x;
   }
 }
@@ -104,6 +104,27 @@ std::vector<double> residual(const MatrixXdr& rhs, const std::vector<Rvector>& a
   return errors;
 }
 
+auto initial_guess(const MatrixXdr& mat, const size_t n_roots) {
+  const auto n = mat.rows();
+  std::vector<Rvector> g;
+  std::vector<Rvector> x;
+  std::vector<size_t> guess;
+  std::vector<double> diagonals;
+  diagonals.reserve(n);
+  for (auto i = 0; i < n; i++) {
+    diagonals.push_back(mat(i, i));
+  }
+  for (size_t root = 0; root < n_roots; root++) {
+    x.emplace_back(n, 0);
+    g.emplace_back(n);
+    auto it_min = std::min_element(diagonals.begin(), diagonals.end());
+    guess.push_back(std::distance(diagonals.begin(), it_min)); // initial guess
+    *std::min_element(diagonals.begin(), diagonals.end()) = 1e99;
+    x.back()[guess.back()] = 1; // initial guess
+  }
+  return std::make_tuple(x, g, guess);
+}
+
 auto set_options(std::shared_ptr<ILinearEquations<Rvector, Qvector, Pvector>>& solver, std::shared_ptr<Logger>& logger,
                  const int n, const int nroot, const int np, bool hermitian, double augmented_hessian) {
   auto options = CastOptions::LinearEquations(solver->get_options());
@@ -127,6 +148,18 @@ auto set_options(std::shared_ptr<ILinearEquations<Rvector, Qvector, Pvector>>& s
   return options;
 }
 
+std::vector<std::vector<double>> matrix_to_vector(const MatrixXdr& rhs, const size_t nRHS) {
+  auto rhs_vector = std::vector<std::vector<double>>{};
+  const auto nX = rhs.rows();
+  for (size_t i = 0; i < nRHS; ++i) {
+    rhs_vector.emplace_back(nX);
+    for (size_t j = 0; j < nX; ++j) {
+      rhs_vector.back()[j] = rhs(j, i);
+    }
+  }
+  return rhs_vector;
+}
+
 void run_test(const MatrixXdr& mat, const MatrixXdr& rhs, const Update& update, double augmented_hessian) {
   auto d = (mat - mat.transpose()).norm();
   bool hermitian = d < 1e-10;
@@ -135,26 +168,42 @@ void run_test(const MatrixXdr& mat, const MatrixXdr& rhs, const Update& update, 
   auto reference_solutions = solve_full_problem(mat, rhs, augmented_hessian);
   auto preconditioner = LinearEquationsPreconditioner(mat);
   for (size_t nroot = 1; nroot <= n_root_max; ++nroot) {
+    auto rhs_vector = matrix_to_vector(rhs, nroot);
     auto [solver, logger] = molpro::test::create_LinearEquations();
     auto options = set_options(solver, logger, mat.rows(), nroot, 0, hermitian, augmented_hessian);
-    // solve the problem iteratively
-    // compare solutions
+    solver->add_equations(molpro::linalg::itsolv::cwrap(rhs_vector));
+    auto [x, g, guess] = initial_guess(mat, solver->n_roots());
+    int nwork = solver->n_roots();
+    int max_iter = 100;
+    size_t n_iter = 1;
+    for (size_t iter = 0; iter < max_iter; ++iter, ++n_iter) {
+      apply_matrix(mat, x, g, nwork);
+      nwork = solver->add_vector(x, g);
+      if (nwork == 0)
+        break;
+      solver->report();
+      preconditioner(g, nwork);
+      nwork = solver->end_iteration(x, g);
+      if (nwork == 0)
+        break;
+    }
     std::vector<std::vector<double>> parameters, actions;
     std::vector<int> roots;
-    //    for (int root = 0; root < solver->n_roots(); root++) {
-    for (int root = 0; root < nroot; root++) {
+    for (int root = 0; root < solver->n_roots(); root++) {
+      //    for (int root = 0; root < nroot; root++) {
       parameters.emplace_back(nX);
       actions.emplace_back(nX);
-      for (size_t i = 0; i < nX; ++i) {
-        parameters[root][i] = reference_solutions[root][i];
-        actions[root][i] = rhs(i, root);
-      }
+      if (false)
+        for (size_t i = 0; i < nX; ++i) {
+          parameters[root][i] = reference_solutions[root][i];
+          actions[root][i] = rhs(i, root);
+        }
       roots.push_back(root);
     }
-    //    solver->solution(roots, parameters, actions);
+    solver->solution(roots, parameters, actions);
     auto residual_errors = residual(rhs, actions);
     EXPECT_EQ(residual_errors.size(), nroot);
-    EXPECT_THAT(residual_errors, ::testing::Each(::testing::Le(1.))); // options->convergence_threshold);
+    EXPECT_THAT(residual_errors, ::testing::Each(::testing::Le(options->convergence_threshold)));
     for (size_t i = 0; i < nroot; ++i) {
       auto norm = std::inner_product(reference_solutions.at(i).begin(), reference_solutions.at(i).end(),
                                      reference_solutions.at(i).begin(), 0.);
@@ -162,7 +211,7 @@ void run_test(const MatrixXdr& mat, const MatrixXdr& rhs, const Update& update, 
         auto overlap_with_reference =
             std::inner_product(parameters.at(i).begin(), parameters.at(i).end(), reference_solutions.at(i).begin(), 0.);
         overlap_with_reference = std::sqrt(std::abs(overlap_with_reference / norm));
-        EXPECT_NEAR(overlap_with_reference, 1., 1) << "root = " << i; // options->convergence_threshold.value());
+        EXPECT_NEAR(overlap_with_reference, 1., options->convergence_threshold.value()) << "root = " << i;
       }
     }
   }
@@ -176,6 +225,8 @@ TEST(LinearEquations, simple_symmetric_system) {
   for (size_t n = 1; n < n_max; ++n) {
     auto [mat, rhs] = construct_simple_symmetric_system(n, p);
     auto update = [](const auto& params, const auto& actions, auto n_work) {};
+    std::cout << "matrix =\n" << mat << std::endl;
+    std::cout << "rhs =\n" << rhs << std::endl;
     run_test(mat, rhs, update, augmented_hessian);
   }
 }

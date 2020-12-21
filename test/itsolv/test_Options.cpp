0d2a524116157cb6fc58257b6594a0794526f711
Marat Sibaev <sibaev.marat@gmail.com>
2020-12-22 10:42:46 +1300

    test copy

--- test/itsolv/test_Options.cpp
+++ test/itsolv/test_Options.cpp
@@ -10,3 +10,15 @@ TEST(Options, default_constructor) {
   ASSERT_FALSE(opt.n_roots);
   ASSERT_FALSE(opt.convergence_threshold);
 }
+
+TEST(Options, copy) {
+  const int n = 3;
+  const double conv_thresh = 1.5e-15;
+  auto opt = Options{};
+  opt.n_roots = n;
+  opt.convergence_threshold = conv_thresh;
+  auto opt_copy = Options();
+  opt_copy.copy(opt);
+  ASSERT_EQ(opt_copy.n_roots, opt.n_roots);
+  ASSERT_EQ(opt_copy.convergence_threshold, opt.convergence_threshold);
+}

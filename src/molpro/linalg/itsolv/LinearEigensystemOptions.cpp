916dc5cab268cd2a0ea78d464a138a1666509324
Marat Sibaev <sibaev.marat@gmail.com>
2020-12-22 14:43:14 +1300

    update LinearEigensystemOptions to use utility functions

--- src/molpro/linalg/itsolv/LinearEigensystemOptions.cpp
+++ src/molpro/linalg/itsolv/LinearEigensystemOptions.cpp
@@ -1,42 +1,29 @@
 #include "LinearEigensystemOptions.h"
-#include <locale>
+#include "options_map.h"
+#include "util.h"
+
 namespace molpro::linalg::itsolv {
 LinearEigensystemOptions::LinearEigensystemOptions(const std::map<std::string, std::string>& opt)
     : ILinearEigensystemOptions(opt) {
-  auto& facet = std::use_facet<std::ctype<char>>(std::locale());
-  auto upper = [&facet](std::string s) {
-    facet.toupper(s.data(), s.data() + s.size());
-    return s;
-  };
-  auto opt_upper = decltype(opt){};
-  for (const auto& [key, val] : opt) {
-    opt_upper[upper(key)] = val;
-  }
-  auto present = [&opt_upper](const auto& key) { return opt_upper.find(key) != opt_upper.end(); };
-  if (auto key = upper("reset_D"); present(key)) {
+  auto facet = util::StringFacet{};
+  auto opt_upper = util::capitalize_keys(opt, facet);
+  if (auto key = facet.toupper("reset_D"); opt_upper.count(key)) {
     reset_D = std::stoi(opt_upper.at(key));
   };
-  if (auto key = upper("reset_D_max_Q_size"); present(key)) {
-    reset_D_max_Q_size = std::stod(opt_upper.at(key));
+  if (auto key = facet.toupper("reset_D_max_Q_size"); opt_upper.count(key)) {
+    reset_D_max_Q_size = std::stoi(opt_upper.at(key));
   }
-  if (auto key = upper("max_size_qspace"); present(key)) {
-    max_size_qspace = std::stod(opt_upper.at(key));
+  if (auto key = facet.toupper("max_size_qspace"); opt_upper.count(key)) {
+    max_size_qspace = std::stoi(opt_upper.at(key));
   }
-  if (auto key = upper("norm_thresh"); present(key)) {
+  if (auto key = facet.toupper("norm_thresh"); opt_upper.count(key)) {
     norm_thresh = std::stod(opt_upper.at(key));
   }
-  if (auto key = upper("svd_thresh"); present(key)) {
+  if (auto key = facet.toupper("svd_thresh"); opt_upper.count(key)) {
     svd_thresh = std::stod(opt_upper.at(key));
   }
-  if (auto key = upper("hermiticity"); present(key)) {
-    auto val = upper(opt_upper.at(key));
-    if (val == "TRUE" || val == "T" || val == "1") {
-      hermiticity = true;
-    } else if (val == "TRUE" || val == "T" || val == "1") {
-      hermiticity = false;
-    } else {
-      throw std::runtime_error("boolean value must be one of {TRUE, FALSE, T, F, 0, 1}");
-    }
+  if (auto key = facet.toupper("hermiticity"); opt_upper.count(key)) {
+    hermiticity = facet.tobool(opt_upper.at(key));
   }
 }
 

74aa0055bcd55a8aaa620fcf18d81bc908748842
Marat Sibaev <sibaev.marat@gmail.com>
2021-01-07 09:22:06 +1300

    update DistrArrayDisk documentation to remove memory view and add chunked local buffer

--- src/molpro/linalg/array/DistrArrayDisk.h
+++ src/molpro/linalg/array/DistrArrayDisk.h
@@ -13,36 +13,19 @@ class Task;
 /*!
  * @brief Distributed array located primarily on disk
  *
- * This class stores the full array on disk. RMA operations read data from disk. A memory view buffer can be allocated
- * which will read the local buffer from disk and keep array in memory until it is flushed. This allows multiple
- * operations on local section to be performed without I/O.
+ * This class stores the full array on disk and implements RMA and more efficient linear algebra operations.
  *
- * Concepts
- * --------
- *   * memory view - a buffer in RAM which mirrors the content of local section of the array on disk.
- *   * a snapshot - a temporary local buffer with contents read directly from disk.
+ * RMA operations read/write directly to disk. Care must be taken that they do not overlap with local buffer
+ * modifications.
  *
- * Differences with DistrArray
- * ---------------------------
+ * LocalBuffer reads the whole local section into memory. This might be prohivitively expensive for large arrays, so
+ * care must be taken.
  *
- * open_access() opens access to the underlying data on disk and close_access() closes that access. If the underlying
- * storage space does not exist than open_access() creates it and reserves necessary space (e.g. creates a file or
- * hdf5 dataset). Both functions are assumed to be collective, although not all implementations might require that.
+ * LocalBufferChunked reads the local section in chunks using a separate thread for I/O. This is more memory efficient
+ * and allows overlap of communication and computation.
  *
- * allocate_buffer() creates the memory view buffer. The buffer will persist until free_buffer() is called. The memory
- * view has to be written to disk by calling flush.
+ * More efficient linear algebra operations are implemented using ChunkedLocalBuffer.
  *
- * LocalBuffer will use the memory view buffer if it was allocated, otherwise it will allocate a new buffer. The new
- * buffer will be a snapshot of the array currently on disk. A non-const local buffer snapshot is always written to disk
- * on destruction, even if it was not modified.
- *
- * Linear algebra operations work by creating a LocalBuffer instance which loads relevant section of the array,
- * followed by numerical operations and optionally dumping of LocalBuffer to disk.
- *
- * There is no default mechanism for keeping memory view in sync. That is, modifications of array on disk via RMA calls
- * makes memory view out of sync.
- *
- * @note The main point of memory view is to minimize IO for linear algebra operations.
  *
  * IO can be done with a thread
  * ----------------------------
@@ -69,30 +52,7 @@ class Task;
  * t.wait(); // wait for the thread to finish the I/O
  * @endcode
  *
- * Advice
- * ------
- *
- * Enclose section of linear algebra calls with  allocate_buffer(), flush() and free_buffer().
- * Operations will be done using memory view buffer without unnecessary IO.
- *
- * Mixing up incompatible RMA and linear algebra operations can lead to undefined behavior of DistrArray. This is also
- * true for DistrArrayDisk and the buffering mechanism where the allocated buffer mimics data stored on disk.
- *
  * @code{.cpp}
- * auto x = DistrArrayDisk{...};
- * auto y = DistrArrayDisk{...};
- * x.open_access();
- * y.open_access();
- * x.allocate_buffer();
- * y.allocate_buffer();
- * auto norm = std::sqrt(x.dot(x));
- * auto ov = x.dot(y);
- * y.axpy(ov / norm, x);
- * y.flush();
- * y.free_buffer();
- * x.free_buffer();
- * x.close_access();
- * y.close_access();
  * @endcode
  *
  *
@@ -127,6 +87,7 @@ public:
   [[nodiscard]] const Distribution &distribution() const override;
 
 protected:
+  //! Loads the whole local buffer from disk
   class LocalBufferDisk : public DistrArray::LocalBuffer {
   public:
     explicit LocalBufferDisk(DistrArrayDisk &source);
@@ -143,9 +104,15 @@ protected:
     DistrArrayDisk &m_source;                  //!< keep a handle on source to dump data to disk
   };
 
+  // TODO The mechanism for loading the file in chunks should be shared with serial disk array
+  //! Loads local buffer in small chunks
+  class LocalBufferChunked {};
+
 public:
   [[nodiscard]] std::unique_ptr<LocalBuffer> local_buffer() override;
   [[nodiscard]] std::unique_ptr<const LocalBuffer> local_buffer() const override;
+  [[nodiscard]] std::unique_ptr<LocalBufferChunked> local_buffer_chunked();
+  [[nodiscard]] std::unique_ptr<const LocalBufferChunked> local_buffer_chunked() const;
 
   //! Writes section of array to disk in a separate thread, bypassing the memory view.
   //! @return Task manager that can test and/or wait for completion
@@ -170,6 +137,10 @@ public:
   std::unique_ptr<util::Task<std::unique_ptr<LocalBuffer>>> tlocal_buffer();
   std::unique_ptr<util::Task<std::unique_ptr<const LocalBuffer>>> tlocal_buffer() const;
 };
+
+double dot(const DistrArrayDisk &x, const DistrArrayDisk &y);
+double dot(const DistrArrayDisk &x, const DistrArray &y);
+double dot(const DistrArray &x, const DistrArrayDisk &y);
 
 } // namespace molpro::linalg::array
 

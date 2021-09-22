f6321cd774d0e3f2a3471575136f15a28faa85c8
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2021-09-22 15:41:14 +0100

    temp_file_name() incorporates static incrementing counter. Fixes #123

--- src/molpro/linalg/array/util/temp_file.cpp
+++ src/molpro/linalg/array/util/temp_file.cpp
@@ -1,17 +1,24 @@
 #include "temp_file.h"
+#include "../util.h"
 #include <cstdlib>
 #include <fstream>
 #include <functional>
+#include <limits>
 #include <thread>
 #include <unistd.h>
-#include <limits>
 
 namespace molpro::linalg::array::util {
 #ifdef HAVE_MPI_H
 fs::path temp_file_name(const fs::path& base_name, const std::string& suffix, MPI_Comm comm) {
   int rank;
   MPI_Comm_rank(comm, &rank);
-  auto fname = rank != 0 ? fs::path{""}.native() : temp_file_name(base_name, suffix).native();
+  auto fname = fs::path{""}.native();
+  {
+#ifdef HAVE_MPI_H
+    molpro::linalg::array::util::LockMPI3 lock(comm);
+#endif
+    fname = rank != 0 ? fs::path{""}.native() : temp_file_name(base_name, suffix).native();
+  }
   int fnamesize = fname.size();
   MPI_Bcast(&fnamesize, 1, MPI_INT, 0, comm);
   fname.resize(fnamesize);
@@ -20,14 +27,18 @@ fs::path temp_file_name(const fs::path& base_name, const std::string& suffix, MP
 }
 #endif
 
+int s_temp_file_name_count = 0;
+std::mutex s_mutex;
 fs::path temp_file_name(const fs::path& base_name, const std::string& suffix) {
+  auto lock = std::lock_guard(s_mutex);
   const std::string chars = "01234566789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const int length = 32;
   std::hash<std::thread::id> hasher;
   auto time = std::chrono::high_resolution_clock().now().time_since_epoch().count();
   auto tid = hasher(std::this_thread::get_id());
   auto pid = getpid();
-  srand(int((time + tid + pid) & std::numeric_limits<int>::max()));
+  s_temp_file_name_count++;
+  srand(int((time + tid + pid + s_temp_file_name_count) & std::numeric_limits<int>::max()));
   auto fname = base_name;
   for (int i = 0; i < length; i++)
     fname += chars[rand() % chars.size()];

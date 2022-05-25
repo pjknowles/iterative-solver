d0c416a56966a870dcfd7ffb22255659a4bab216
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-05-25 18:48:37 +0100

    Fix OptimizeExampleF

--- examples/OptimizeExampleF.F90
+++ examples/OptimizeExampleF.F90
@@ -20,19 +20,17 @@ PROGRAM QuasiNewton_Example
   DOUBLE PRECISION, DIMENSION (n, n) :: m
   DOUBLE PRECISION, DIMENSION (n) :: c, g
   DOUBLE PRECISION :: e, e0
-  DOUBLE PRECISION, DIMENSION(1) :: error
   INTEGER :: i, j
   LOGICAL :: converged
   LOGICAL, PARAMETER :: forced = .FALSE.
   call mpi_init
   PRINT *, 'Fortran binding of IterativeSolver::IOptimize'
-  m = 1; DO i = 1, n; m(i, i) = 3*i;
+  m = 1; DO i = 1, n; m(i, i) = 3 * i;
   END DO
-  CALL Iterative_Solver_Optimize_Initialize(n, thresh = 1d-9, verbosity = 1, algorithm = "L-BFGS")
-  c = 0; if (.not. forced) c(1) = 1
+  CALL Iterative_Solver_Optimize_Initialize(n, thresh = 1d-9, verbosity = 1, algorithm = "BFGS")
+  c = 0; if ( .not. forced) c(1) = 1
   e0 = m(1, 1)
   DO i = 1, 30
-        c = c / sqrt(dot_product(c, c))
     g = MATMUL(m, c)
     if (forced) then
       e = 0.5 * dot_product(c, g) - sum(c)
@@ -41,19 +39,19 @@ PROGRAM QuasiNewton_Example
       e = dot_product(c, g) / dot_product(c, c)
       g = (g - e * c) / dot_product(c, c)
     end if
-    write (6, *) 'function value ',e
-        write (6,*) 'c ',c
-        write (6,*) 'g ',g
+    write (6, *) 'function value ', e
+    write (6, *) 'c ', c
+    write (6, *) 'g ', g
     IF (Iterative_Solver_Add_Value(e, c, g)) THEN
       if (forced) then
         g = g / [(m(j, j), j = 1, n)]
       else
         g = g / ([(m(j, j), j = 1, n)] - e + 1d-15) &
-            + (sum([(c(j)*g(j),j=1,n)])/sum([(c(j)**2,j=1,n)])) * c &
-                / ([(m(j, j), j = 1, n)] - e + 1d-15)
+          + (sum([(c(j) * g(j), j = 1, n)]) / sum([(c(j)**2, j = 1, n)])) * c &
+            / ([(m(j, j), j = 1, n)] - e + 1d-15)
       end if
     END IF
-    converged = Iterative_Solver_End_Iteration(c, g, error)
+    converged = Iterative_Solver_End_Iteration(c, g) .eq.0
     IF (converged) EXIT
   END DO
   if (.not. forced) c = c / sqrt(dot_product(c, c))

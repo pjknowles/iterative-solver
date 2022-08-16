71533d2a202803f952ac3931fb1f4fcd7b43c5a4
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-08-16 06:46:30 +0100

    BufferManager header-only

--- src/molpro/linalg/array/util/BufferManager.h
+++ src/molpro/linalg/array/util/BufferManager.h
@@ -5,6 +5,7 @@
 #include <future>
 #include <molpro/linalg/array/DistrArray.h>
 #include <vector>
+#include <molpro/linalg/array/util/Distribution.h>
 
 namespace molpro::linalg::array::util {
 
@@ -36,29 +37,36 @@ public:
    * @param buffer_size number of array elements in each chunk.
    * @param number_of_buffers how many buffers.
    */
-  BufferManager(const CVecRef<DistrArrayFile>& arrays, size_t buffer_size = 8192, int number_of_buffers = 2);
-//  BufferManager(const std::vector<DistrArray>& arrays, size_t buffer_size = 8192, int number_of_buffers = 2)
-//      : BufferManager(cwrap(arrays), buffer_size, number_of_buffers) {}
-//  BufferManager(const DistrArray& array, size_t buffer_size = 8192, int number_of_buffers = 2)
-//      : BufferManager(CVecRef<DistrArray>{std::cref(array)}, buffer_size, number_of_buffers) {}
+  BufferManager(const CVecRef<DistrArrayFile>& arrays, size_t buffer_size = 8192, int number_of_buffers = 2)
+      : m_arrays(arrays), m_buffer_size(buffer_size), m_number_of_buffers(number_of_buffers),
+        m_range(arrays.empty() ? std::pair<DistrArray::index_type, DistrArray::index_type>{0, 0}
+                               : arrays.front().get().distribution().range(molpro::mpi::rank_global())),
+        m_buffer(arrays.size() * number_of_buffers * buffer_size) {
+    assert(number_of_buffers > 0 && number_of_buffers < 3);
+  }
+
+  //  BufferManager(const std::vector<DistrArray>& arrays, size_t buffer_size = 8192, int number_of_buffers = 2)
+  //      : BufferManager(cwrap(arrays), buffer_size, number_of_buffers) {}
+  //  BufferManager(const DistrArray& array, size_t buffer_size = 8192, int number_of_buffers = 2)
+  //      : BufferManager(CVecRef<DistrArray>{std::cref(array)}, buffer_size, number_of_buffers) {}
 
   /*!
    * The current buffer size
    * @return
    */
-  size_t buffer_size() const;
+  size_t buffer_size() const { return m_current_buffer_size; }
   /*!
    * The distance between buffers
    * @return
    */
-  size_t buffer_stride() const;
+  size_t buffer_stride() const { return m_buffer_size; }
   /*!
    * Offset of the current buffer
    * @return
    */
-  size_t buffer_offset() const;
-protected:
+  size_t buffer_offset() const { return m_current_segment * m_buffer_size; }
 
+protected:
   using value_type = DistrArray::value_type;
   const CVecRef<DistrArrayFile>& m_arrays; // reference to the DistrArray objects data is being accessed from
   const size_t m_buffer_size = 8192;
@@ -126,7 +134,45 @@ protected:
    * @param initial whether this is the first iteration
    * @return the iterator value for the next iteration
    */
-  [[nodiscard]] Span<value_type> next(bool initial = false);
+  [[nodiscard]] Span<value_type> next(bool initial = false) {
+    auto prof = molpro::Profiler::single()->push(std::string{"BufferManager::next("} + (initial ? "T" : "F") + ")");
+    if (initial)
+      m_current_segment = 0;
+    else
+      ++m_current_segment;
+    const auto buffer_id = m_current_segment % m_number_of_buffers;
+    const auto lo = m_range.first + m_current_segment * this->m_buffer_size;
+    if (lo >= m_range.second)
+      return Span<BufferManager::value_type>(nullptr, 0);
+    const auto hi = std::min(lo + this->m_buffer_size, m_range.second);
+    m_current_buffer_size = hi - lo;
+    {
+      if (m_number_of_buffers == 1 or lo == m_range.first) {
+        for (size_t iarray = 0; iarray < m_arrays.size(); ++iarray) {
+          assert(hi > lo);
+          assert(buffer_id * m_buffer_size * m_arrays.size() + iarray * m_buffer_size + hi - lo < m_buffer.size());
+          m_arrays[iarray].get().get(
+              lo, hi, &m_buffer.at(buffer_id * m_buffer_size * m_arrays.size() + iarray * m_buffer_size));
+        }
+      } else {
+        m_read_future.wait();
+      }
+    }
+
+    const auto next_buffer_id = (m_current_segment + 1) % m_number_of_buffers;
+    const auto next_lo = m_range.first + (m_current_segment + 1) * this->m_buffer_size;
+    const auto next_hi = std::min(next_lo + this->m_buffer_size, m_range.second);
+    if (m_number_of_buffers > 1 and next_lo < m_range.second) {
+      m_read_future = std::async(std::launch::async, [this, next_lo, next_hi, next_buffer_id]() {
+        for (size_t iarray = 0; iarray < m_arrays.size(); ++iarray)
+          m_arrays[iarray].get().get(
+              next_lo, next_hi,
+              &m_buffer.at(next_buffer_id * m_buffer_size * m_arrays.size() + iarray * m_buffer_size));
+      });
+    }
+
+    return Span<value_type>(&m_buffer.at(buffer_id * m_buffer_size * m_arrays.size()), m_buffer_size * m_arrays.size());
+  }
 
   size_t m_current_segment = 0; // current chunk
   const std::pair<size_t, size_t>

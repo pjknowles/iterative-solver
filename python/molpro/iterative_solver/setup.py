d01dbfb3e0b4e6ccbd671201065e9c1f54d15994
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-05-07 10:33:36 +0100

    More setup

--- python/molpro/iterative_solver/setup.py
+++ python/molpro/iterative_solver/setup.py
@@ -1,32 +1,40 @@
-# from distutils.core import setup
-# from distutils.extension import Extension
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
-
 import subprocess
-subprocess.run("""
-set -e
-mkdir -p build/cmake-build
-cd build/cmake-build
-cmake -DCMAKE_BUILD_TYPE=Release -DDEPENDENCYMANAGER_FETCHCONTENT=ON -DLINEARALGEBRA_ARRAY_HDF5=OFF -DFORTRAN=OFF -DBUILD_SHARED_LIBS=ON ../../../../..
-cmake --build . --config Release
-pwd
-find . \( -name '*.dylib' -o -name '*.so' \) -exec ls -la {} \;
-find . \( -name '*.dylib' -o -name '*.so' \) -exec cp -p {} .. \;
-""", shell=True, check=True)
+import shutil
+import pathlib
+
+current_ = pathlib.Path(__file__).parent.resolve()
+build_ = (current_ / 'build' / 'cmake-build')
+build_.mkdir(parents=True, exist_ok=True)
+source_ = current_.parent.parent.parent.resolve()
+subprocess.run(
+    [
+        'cmake',
+        '-DCMAKE_BUILD_TYPE=Release',
+        '-DDEPENDENCYMANAGER_FETCHCONTENT=ON', '-DLINEARALGEBRA_ARRAY_HDF5=OFF', '-DFORTRAN=OFF',
+        '-DBUILD_SHARED_LIBS=ON',
+        '-S', str(source_), '-B', str(build_),
+    ],
+    shell=False)
+subprocess.run(['cmake', '--build', str(build_), '--config', 'Release'], shell=False)
+for suffix in ['so', 'dylib']:
+    for path in pathlib.Path(build_).glob('**/*.'+suffix):
+        shutil.copy2(str(path), str(build_.parent))
 
 ext = Extension('iterative_solver',
                 sources=["iterative_solver.pyx"],
                 language="c++",
-                include_dirs=[numpy.get_include(), '../../../src','../../../dependencies/profiler/src','../../../dependencies/utilities/src'],
+                include_dirs=[numpy.get_include(), '../../../src', '../../../dependencies/profiler/src',
+                              '../../../dependencies/utilities/src'],
                 extra_compile_args=["-std=c++17"],
                 define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-                libraries=["iterative-solver","utilities"],
+                libraries=["iterative-solver", "utilities"],
                 library_dirs=[
                     "build",
-                #     "../../../cmake-build-release-shared/src",
-                #     "../../../cmake-build-release-shared/_deps/utilities-build/src",
+                    #     "../../../cmake-build-release-shared/src",
+                    #     "../../../cmake-build-release-shared/_deps/utilities-build/src",
                 ]
                 )
 

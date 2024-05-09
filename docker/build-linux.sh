7ad93e8ee70de97358eb67947ad5399e5e4cbf5c
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-05-09 11:14:56 +0100

    support for build module

---
+++ docker/build-linux.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+set -e
+platforms='arm64 amd64'
+root=$(realpath $(dirname $0)/..)
+for platform in $platforms; do
+  echo platform=$platform
+  (
+    cd $root/docker
+    docker build . -t iterative-solver-$platform --platform linux/$platform
+  )
+  mkdir -p build-$platform
+  docker run --rm -v $PWD:$PWD -v $PWD/build-$platform:$PWD/build -w $PWD \
+    --platform linux/$platform iterative-solver-$platform \
+    python -m build
+done

04684b0f67c239d07055ae4d87d7c4174ab517bd
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-05-16 07:51:46 +0100

    tidy

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
-# import os
-
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 import subprocess
-import shutil
 import pathlib
 
 root_dir_ = pathlib.Path(__file__).parent.resolve()
@@ -16,24 +13,16 @@ cmake_build_dir_.mkdir(parents=True, exist_ok=True)
 subprocess.run(
     [
         'cmake',
-        '-DCMAKE_CXX_FLAGS=-fPIC',
+        '-DCMAKE_CXX_FLAGS=-fPIC',  # TODO make portable
         '-DCMAKE_BUILD_TYPE=Release',
-        '-DDEPENDENCYMANAGER_FETCHCONTENT=OFF', '-DLINEARALGEBRA_ARRAY_HDF5=OFF', '-DLINEARALGEBRA_ARRAY_GA=OFF',
+        '-DDEPENDENCYMANAGER_FETCHCONTENT=OFF',
+        '-DLINEARALGEBRA_ARRAY_HDF5=OFF', '-DLINEARALGEBRA_ARRAY_GA=OFF',
         '-DFORTRAN=OFF',
         '-DBUILD_SHARED_LIBS=OFF', '-DLIBRARY_ONLY=ON',
-        # '-DCMAKE_MACOSX_RPATH=OFF',
-        # '--trace',
         '-S', str(root_dir_), '-B', str(cmake_build_dir_),
     ],
-    # env=dict(os.environ)|{
-    #     'CXX':'/Users/peterk/.local/mpich/bin/mpicxx',
-    #     'CC':'/Users/peterk/.local/mpich/bin/mpicc',
-    #      },
     shell=False)
 subprocess.run(['cmake', '--build', str(cmake_build_dir_), '-v', '--config', 'Release'], shell=False)
-for suffix in ['so', 'dylib', 'a']:
-    for path in pathlib.Path(cmake_build_dir_).glob('**/*.' + suffix):
-        shutil.copy2(str(path), str(cmake_build_dir_.parent))
 
 ext = Extension('iterative_solver',
                 sources=["src/iterative_solver/iterative_solver.pyx"],
@@ -44,14 +33,12 @@ ext = Extension('iterative_solver',
                               ],
                 extra_compile_args=["-std=c++17"],
                 extra_objects=[
-                    "build/libiterative-solver.a",
-                    "build/libutilities.a",
-                    "build/libprofiler.a",
-                    # "/Users/peterk/.local/mpich/lib/libmpi.a",
+                    "build/cmake-build/src/libiterative-solver.a",
+                    "build/cmake-build/_deps/utilities-build/src/libutilities.a",
+                    "build/cmake-build/_deps/profiler-build/src/libprofiler.a",
                 ],
                 define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-                libraries=["iterative-solver", "utilities", "blas", "mpi"],
-                library_dirs=[str(cmake_build_dir_.parent)]
+                libraries=["blas", "mpi"],
                 )
 
 setup(

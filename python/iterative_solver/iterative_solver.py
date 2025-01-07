df3dd24af834a261f929388fdcb1ab5c121c870f
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-01-07 16:37:38 +0000

    test_problem_gradient

--- python/iterative_solver/iterative_solver.py
+++ python/iterative_solver/iterative_solver.py
@@ -1,6 +1,7 @@
 from .problem import Problem
 from .iterative_solver_extension import Optimize, LinearEigensystem, LinearEquations, NonLinearEquations
 from .natural_coordinate import NaturalCoordinateProblem
+import numpy as np
 
 
 def Solve_Linear_Eigensystem(parameters, actions, problem: Problem, nroot=1, generate_initial_guess=True,
@@ -31,3 +32,25 @@ def Solve_Optimization(parameters, actions, problem: Problem, generate_initial_g
     solver = Optimize(parameters.shape[-1], **kwargs)
     solver.solve(parameters, actions, problem, generate_initial_guess=generate_initial_guess, max_iter=max_iter)
     return solver
+
+
+def test_problem_gradient(problem: Problem, dimension=1, step=1e-3, rtol=1e-7, atol=1e-13, instance=0):
+    x = np.zeros(dimension)
+    g_problem = np.zeros_like(x)
+    g_numerical = np.zeros_like(x)
+    if not hasattr(problem, 'test_parameters') or not problem.test_parameters(instance=instance, parameters=x):
+        return False
+    v0 = problem.residual(x, g_problem)
+    for i in range(dimension):
+        g = np.zeros_like(x)
+        x[i] -= 2 * step
+        vmm = problem.residual(x, g)
+        x[i] += step
+        vm = problem.residual(x, g)
+        x[i] += 2 * step
+        vp = problem.residual(x, g)
+        x[i] += step
+        vpp = problem.residual(x, g)
+        x[i] -= 2 * step
+        g_numerical[i] = (vmm - 8 * vm + 8 * vp - vpp) / (12 * step)
+    np.testing.assert_allclose(g_problem, g_numerical, rtol=rtol, atol=atol)

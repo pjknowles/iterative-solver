294c7e4b61d34b5bf57d2fedf7ab1aa04a8754e7
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-01-07 16:39:43 +0000

    test_problem_gradient

--- python/iterative_solver/__init__.py
+++ python/iterative_solver/__init__.py
@@ -3,6 +3,7 @@ from .matrix_problem import MatrixProblem
 from .pspace import PSpace
 from .iterative_solver_extension import IterativeSolver, Optimize, LinearEigensystem, LinearEquations, NonLinearEquations
 from .iterative_solver import Solve_Optimization, Solve_Linear_Eigensystem, Solve_Linear_Equations, Solve_NonLinear_Equations
+from .iterative_solver import test_problem_gradient
 from ._version import __version__
 from .natural_coordinate import NaturalCoordinateProblem
 __all__ = ["example_problems"]

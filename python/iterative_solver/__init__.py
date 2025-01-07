2ce4bb925b8a374c776d10106f1a31b3fbdf22e3
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-01-07 15:49:07 +0000

    example_problems

--- python/iterative_solver/__init__.py
+++ python/iterative_solver/__init__.py
@@ -4,3 +4,4 @@ from .pspace import PSpace
 from .iterative_solver_extension import IterativeSolver, Optimize, LinearEigensystem, LinearEquations, NonLinearEquations
 from .iterative_solver import Solve_Optimization, Solve_Linear_Eigensystem, Solve_Linear_Equations, Solve_NonLinear_Equations
 from ._version import __version__
+__all__ = ["example_problems"]
